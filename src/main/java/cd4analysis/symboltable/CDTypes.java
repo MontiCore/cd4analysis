c629c7e3e1ff8e2a0f18b985e61febc403347848
volkova <volkova@3ddcf219-3c89-4d16-bbf6-790a9aa18b9b>
2015-02-10 15:45:47 +0000

    add collection for primitive and wrapper types
    git-svn-id: https://sselab.de/lab2/private/svn/MontiCore/trunk/mclang/cd4analysis@21665 3ddcf219-3c89-4d16-bbf6-790a9aa18b9b

---
+++ src/main/java/cd4analysis/symboltable/CDTypes.java
@@ -0,0 +1,79 @@
+/*
+ * Copyright (c) 2015 RWTH Aachen. All rights reserved.
+ *
+ * http://www.se-rwth.de/
+ */
+package cd4analysis.symboltable;
+
+import java.util.Arrays;
+import java.util.Collection;
+
+/**
+ * TODO: Change me!
+ *
+ * @author (last commit) $Author$
+ * @version $Revision$, $Date$
+ */
+public class CDTypes {
+  
+  // Wrapper types
+  public final static String Integer = "java.lang.Integer";
+  
+  public final static String Double = "java.lang.Double";
+  
+  public final static String Short = "java.lang.Short";
+  
+  public final static String Byte = "java.lang.Byte";
+  
+  public final static String Float = "java.lang.Float";
+  
+  public final static String Long = "java.lang.Long";
+  
+  public final static String Character = "java.lang.Character";
+  
+  public final static String Boolean = "java.lang.Boolean";
+  
+  // Primitive types
+  public final static String IntPimitive = "int";
+  
+  public final static String DoublePimitive = "double";
+  
+  public final static String ShortPimitive = "short";
+  
+  public final static String BytePimitive = "byte";
+  
+  public final static String FloatPimitive = "float";
+  
+  public final static String LongPimitive = "long";
+  
+  public final static String CharPimitive = "char";
+  
+  public final static String BooleanPimitive = "boolean";
+  
+  public static Collection<String> wrapperTypes = Arrays.asList(
+      Integer, Double, Short, Byte, Float, Long, Character, Boolean);
+  
+  public static Collection<String> primitiveTypes = Arrays.asList(
+      IntPimitive, DoublePimitive, ShortPimitive, BytePimitive, Float, LongPimitive, CharPimitive,
+      BooleanPimitive);
+  
+  /**
+   * Checks is the given type an implicit type
+   * 
+   * @return
+   */
+  public static boolean isWrapperType(String typeName) {
+    return wrapperTypes.contains(typeName)
+        || (typeName.lastIndexOf('.') == -1 && wrapperTypes.contains("java.lang." + typeName));
+  }
+  
+  /**
+   * Checks is the given type a primitive type
+   * 
+   * @return
+   */
+  public static boolean isPrimitiveType(String typeName) {
+    return primitiveTypes.contains(typeName);
+  }
+  
+}
