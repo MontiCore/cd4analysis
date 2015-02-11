67e7bfe8de5b4f4c5a3a3c43fec7e76a2593fe1b
volkova <volkova@3ddcf219-3c89-4d16-bbf6-790a9aa18b9b>
2015-02-11 16:53:42 +0000

    added some help methods
    git-svn-id: https://sselab.de/lab2/private/svn/MontiCore/trunk/mclang/cd4analysis@21717 3ddcf219-3c89-4d16-bbf6-790a9aa18b9b

--- src/main/java/cd4analysis/symboltable/CDTypes.java
+++ src/main/java/cd4analysis/symboltable/CDTypes.java
@@ -15,62 +15,62 @@ import java.util.Collection;
  * @version $Revision$, $Date$
  */
 public class CDTypes {
-
+  
   // Wrapper types
   public final static String Integer = "java.lang.Integer";
-
+  
   public final static String Double = "java.lang.Double";
-
+  
   public final static String Short = "java.lang.Short";
-
+  
   public final static String Byte = "java.lang.Byte";
-
+  
   public final static String Float = "java.lang.Float";
-
+  
   public final static String Long = "java.lang.Long";
-
+  
   public final static String Character = "java.lang.Character";
-
+  
   public final static String Boolean = "java.lang.Boolean";
-
+  
   // Primitive types
   public final static String IntPimitive = "int";
-
+  
   public final static String DoublePimitive = "double";
-
+  
   public final static String ShortPimitive = "short";
-
+  
   public final static String BytePimitive = "byte";
-
+  
   public final static String FloatPimitive = "float";
-
+  
   public final static String LongPimitive = "long";
-
+  
   public final static String CharPimitive = "char";
-
+  
   public final static String BooleanPimitive = "boolean";
-
+  
   // Well known types
   public final static String String = "java.lang.String";
   
   public final static String JAVA_LANG_PACKAGE = "java.lang.";
-
+  
   // Collection types
   public final static String Collection = "java.util.Collection";
-
+  
   public final static String List = "java.util.List";
-
+  
   public final static String Map = "java.util.Map";
-
+  
   public static Collection<String> wrapperTypes = Arrays.asList(Integer,
       Double, Short, Byte, Float, Long, Character, Boolean);
-
+  
   public static Collection<String> primitiveTypes = Arrays.asList(IntPimitive,
       DoublePimitive, ShortPimitive, BytePimitive, FloatPimitive, LongPimitive,
       CharPimitive, BooleanPimitive);
-
+  
   public static Collection<String> dataTypes = Arrays.asList(String);
-
+  
   public static Collection<String> collectionTypes = Arrays.asList(Collection,
       List, Map);
   
@@ -84,7 +84,7 @@ public class CDTypes {
         || (typeName.lastIndexOf('.') == -1 && wrapperTypes
             .contains(JAVA_LANG_PACKAGE + typeName));
   }
-
+  
   /**
    * Checks is the given type a primitive type
    * 
@@ -98,7 +98,8 @@ public class CDTypes {
    * Converts the given primitive type to the wrapper type
    * 
    * @param primitiveType
-   * @return wrapper type if the given type is a primitive type else the given type
+   * @return wrapper type if the given type is a primitive type else the given
+   * type
    */
   public static String primitiveToWrapper(String primitiveType) {
     String wrapperName = primitiveType;
@@ -129,7 +130,8 @@ public class CDTypes {
    * Converts the given primitive type to the wrapper type
    * 
    * @param primitiveType
-   * @return wrapper type if the given type is a primitive type else the given type
+   * @return wrapper type if the given type is a primitive type else the given
+   * type
    */
   public static String primitiveToQualifiedWrapper(String primitiveType) {
     switch (primitiveType) {
@@ -158,7 +160,8 @@ public class CDTypes {
    * Converts the given wrapper type to the primitive type
    * 
    * @param wrapperType
-   * @return primitive type if the given type is a wrapper type else the given type
+   * @return primitive type if the given type is a wrapper type else the given
+   * type
    */
   public static String wrapperToPrimitive(String wrapperType) {
     if (wrapperType.indexOf('.') == -1) {
@@ -185,14 +188,56 @@ public class CDTypes {
         return wrapperType;
     }
   }
-
+  
   public static boolean isString(CDTypeSymbol typeSymbol) {
-    boolean returnValue = String.equals(typeSymbol.getFullName())|| (typeSymbol.getFullName().lastIndexOf('.') == -1 && String.equals("java.lang." + typeSymbol.getFullName()));
+    boolean returnValue = String.equals(typeSymbol.getFullName())
+        || (typeSymbol.getFullName().lastIndexOf('.') == -1 && String.equals("java.lang."
+            + typeSymbol.getFullName()));
     return returnValue;
   }
-
+  
   public static boolean isCollectionType(CDTypeSymbol typeSymbol) {
     return collectionTypes.contains(typeSymbol.getFullName());
   }
-
+  
+  public static boolean isInteger(String typeName) {
+    return IntPimitive.equals(typeName)
+        || (typeName.lastIndexOf('.') == -1 && Integer.equals(JAVA_LANG_PACKAGE + typeName));
+  }
+  
+  public static boolean isDouble(String typeName) {
+    return DoublePimitive.equals(typeName)
+        || (typeName.lastIndexOf('.') == -1 && Double.equals(JAVA_LANG_PACKAGE + typeName));
+  }
+  
+  public static boolean isShort(String typeName) {
+    return ShortPimitive.equals(typeName)
+        || (typeName.lastIndexOf('.') == -1 && Short.equals(JAVA_LANG_PACKAGE + typeName));
+  }
+  
+  public static boolean isByte(String typeName) {
+    return BytePimitive.equals(typeName)
+        || (typeName.lastIndexOf('.') == -1 && Byte.equals(JAVA_LANG_PACKAGE + typeName));
+  }
+  
+  public static boolean isFloat(String typeName) {
+    return FloatPimitive.equals(typeName)
+        || (typeName.lastIndexOf('.') == -1 && Float.equals(JAVA_LANG_PACKAGE + typeName));
+  }
+  
+  public static boolean isLong(String typeName) {
+    return LongPimitive.equals(typeName)
+        || (typeName.lastIndexOf('.') == -1 && Long.equals(JAVA_LANG_PACKAGE + typeName));
+  }
+  
+  public static boolean isCharacter(String typeName) {
+    return CharPimitive.equals(typeName)
+        || (typeName.lastIndexOf('.') == -1 && Character.equals(JAVA_LANG_PACKAGE + typeName));
+  }
+  
+  public static boolean isBoolean(String typeName) {
+    return BooleanPimitive.equals(typeName)
+        || (typeName.lastIndexOf('.') == -1 && Boolean.equals(JAVA_LANG_PACKAGE + typeName));
+  }
+  
 }
