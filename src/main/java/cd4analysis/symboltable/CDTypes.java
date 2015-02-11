9d16ae66c715cf981a3682928f7856db477ec1d9
eikermann <eikermann@3ddcf219-3c89-4d16-bbf6-790a9aa18b9b>
2015-02-11 13:53:48 +0000

    added isCollection test
    git-svn-id: https://sselab.de/lab2/private/svn/MontiCore/trunk/mclang/cd4analysis@21702 3ddcf219-3c89-4d16-bbf6-790a9aa18b9b

--- src/main/java/cd4analysis/symboltable/CDTypes.java
+++ src/main/java/cd4analysis/symboltable/CDTypes.java
@@ -15,54 +15,63 @@ import java.util.Collection;
  * @version $Revision$, $Date$
  */
 public class CDTypes {
-  
+
   // Wrapper types
   public final static String Integer = "java.lang.Integer";
-  
+
   public final static String Double = "java.lang.Double";
-  
+
   public final static String Short = "java.lang.Short";
-  
+
   public final static String Byte = "java.lang.Byte";
-  
+
   public final static String Float = "java.lang.Float";
-  
+
   public final static String Long = "java.lang.Long";
-  
+
   public final static String Character = "java.lang.Character";
-  
+
   public final static String Boolean = "java.lang.Boolean";
-  
+
   // Primitive types
   public final static String IntPimitive = "int";
-  
+
   public final static String DoublePimitive = "double";
-  
+
   public final static String ShortPimitive = "short";
-  
+
   public final static String BytePimitive = "byte";
-  
+
   public final static String FloatPimitive = "float";
-  
+
   public final static String LongPimitive = "long";
-  
+
   public final static String CharPimitive = "char";
-  
+
   public final static String BooleanPimitive = "boolean";
-  
-  //Well known types
+
+  // Well known types
   public final static String String = "java.lang.String";
-  
-  
-  public static Collection<String> wrapperTypes = Arrays.asList(
-      Integer, Double, Short, Byte, Float, Long, Character, Boolean);
-  
-  public static Collection<String> primitiveTypes = Arrays.asList(
-      IntPimitive, DoublePimitive, ShortPimitive, BytePimitive, Float, LongPimitive, CharPimitive,
-      BooleanPimitive);
-  
+
+  // Collection types
+  public final static String Collection = "java.util.Collection";
+
+  public final static String List = "java.util.List";
+
+  public final static String Map = "java.util.Map";
+
+  public static Collection<String> wrapperTypes = Arrays.asList(Integer,
+      Double, Short, Byte, Float, Long, Character, Boolean);
+
+  public static Collection<String> primitiveTypes = Arrays.asList(IntPimitive,
+      DoublePimitive, ShortPimitive, BytePimitive, Float, LongPimitive,
+      CharPimitive, BooleanPimitive);
+
   public static Collection<String> dataTypes = Arrays.asList(String);
-  
+
+  public static Collection<String> collectionTypes = Arrays.asList(Collection,
+      List, Map);
+
   /**
    * Checks is the given type an implicit type
    * 
@@ -70,9 +79,10 @@ public class CDTypes {
    */
   public static boolean isWrapperType(String typeName) {
     return wrapperTypes.contains(typeName)
-        || (typeName.lastIndexOf('.') == -1 && wrapperTypes.contains("java.lang." + typeName));
+        || (typeName.lastIndexOf('.') == -1 && wrapperTypes
+            .contains("java.lang." + typeName));
   }
-  
+
   /**
    * Checks is the given type a primitive type
    * 
@@ -81,12 +91,13 @@ public class CDTypes {
   public static boolean isPrimitiveType(String typeName) {
     return primitiveTypes.contains(typeName);
   }
-  
+
   public static boolean isString(CDFieldSymbol fieldSymbol) {
-    System.err.println("######################################");
-    System.err.println(fieldSymbol.getFullName());
-    System.err.println("######################################");
     return String.equals(fieldSymbol.getFullName());
   }
-  
+
+  public static boolean isCollectionType(CDFieldSymbol fieldSymbol) {
+    return collectionTypes.contains(fieldSymbol.getFullName());
+  }
+
 }
