408b296f0b591c22e2fb6b7404a164dafc2c8e32
heim <heim@3ddcf219-3c89-4d16-bbf6-790a9aa18b9b>
2015-04-02 17:59:43 +0000

    minor
    git-svn-id: https://sselab.de/lab2/private/svn/MontiCore/trunk/mclang/cd4analysis@22955 3ddcf219-3c89-4d16-bbf6-790a9aa18b9b

--- src/test/java/de/monticore/umlcd4a/parser/CD4AnalysisParserTest.java
+++ src/test/java/de/monticore/umlcd4a/parser/CD4AnalysisParserTest.java
@@ -11,10 +11,6 @@ import static org.junit.Assert.assertTrue;
 import java.io.IOException;
 import java.nio.file.Path;
 import java.nio.file.Paths;
-import java.util.ArrayList;
-import java.util.Collections;
-
-import mc.ast.ASTNode;
 
 import org.antlr.v4.runtime.RecognitionException;
 import org.junit.Test;
@@ -27,30 +23,6 @@ import de.monticore.umlcd4a._parser.CDCompilationUnitMCParser;
 
 public class CD4AnalysisParserTest {
   
-  private int getDepth(ASTNode n) {
-    ArrayList<Integer> depths = new ArrayList<>();
-    for (ASTNode c : n.get_Children()) {
-      depths.add(getDepth(c));
-    }
-    if (depths.isEmpty()) {
-      return 0;
-    }
-
-    return Collections.max(depths) + 1;
-  }
-
-  private int getWidth(ASTNode n) {
-    int width = 0;
-    for (ASTNode c : n.get_Children()) {
-      width+=1;
-      int w  = getWidth(c);
-      if (w>width) {
-        width = w;
-      }
-    }
-    return width;
-  }
-
   @Test
   public void testSocNet() throws RecognitionException, IOException {
     Path model = Paths.get("src/test/resources/de/monticore/umlcd4a/parser/SocNet.cd");
@@ -58,11 +30,6 @@ public class CD4AnalysisParserTest {
     Optional<ASTCDCompilationUnit> cdDef = parser.parse(model.toString());
     assertFalse(parser.hasErrors());
     assertTrue(cdDef.isPresent());
-    System.out.println();
-    System.out.println(getDepth(cdDef.get()));
-    System.out.println();
-    System.out.println(getWidth(cdDef.get()));
-    
   }
   
   @Test
