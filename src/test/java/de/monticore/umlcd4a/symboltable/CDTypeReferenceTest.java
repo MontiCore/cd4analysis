df7bc4ae1301c9870a22ee9d4762acb6d4b4c0d5
nazari <nazari@3ddcf219-3c89-4d16-bbf6-790a9aa18b9b>
2015-06-19 07:19:05 +0000

    added test for qualified type references
    git-svn-id: https://sselab.de/lab2/private/svn/MontiCore/trunk/mclang/cd4analysis@24273 3ddcf219-3c89-4d16-bbf6-790a9aa18b9b

--- src/test/java/de/monticore/umlcd4a/symboltable/CDTypeReferenceTest.java
+++ src/test/java/de/monticore/umlcd4a/symboltable/CDTypeReferenceTest.java
@@ -9,16 +9,37 @@ import static org.junit.Assert.assertEquals;
 import static org.junit.Assert.assertTrue;
 
 import de.monticore.symboltable.GlobalScope;
+import de.monticore.symboltable.MutableScope;
 import de.monticore.umlcd4a.symboltable.references.CDTypeSymbolReference;
 import org.junit.Test;
 
 public class CDTypeReferenceTest {
 
   @Test
-  public void test() {
+  public void testQualifiedTypeReferenceFromWithinGlobalScope() {
     GlobalScope globalScope = CD4AGlobalScopeTestFactory.create();
 
     CDTypeSymbolReference ref = new CDTypeSymbolReference("de.monticore.umlcd4a.symboltable.CD2.Person", globalScope);
+
+    assertTrue(ref.existsReferencedSymbol());
+    assertEquals("de.monticore.umlcd4a.symboltable.CD2.Person", ref.getFullName());
+    assertEquals("Person", ref.getName());
+  }
+
+  @Test
+  public void testQualifiedTypeReferenceFromWithinAnCD() {
+    GlobalScope globalScope = CD4AGlobalScopeTestFactory.create();
+
+    CDSymbol cd = new CDSymbol("CD");
+    globalScope.define(cd);
+
+    MutableScope cdScope = (MutableScope) cd.getSpannedScope();
+    cdScope.setResolvingFilters(globalScope.getResolvingFilters());
+
+    CDTypeSymbol type = new CDTypeSymbol("Person");
+    cdScope.define(type);
+
+    CDTypeSymbolReference ref = new CDTypeSymbolReference("de.monticore.umlcd4a.symboltable.CD2.Person", cd.getSpannedScope());
 
     assertTrue(ref.existsReferencedSymbol());
     assertEquals("de.monticore.umlcd4a.symboltable.CD2.Person", ref.getFullName());
