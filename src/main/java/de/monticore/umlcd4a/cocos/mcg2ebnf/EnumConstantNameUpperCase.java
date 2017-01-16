e06f63b324c8d4cc23edd99fd56a6172b851be92
roth <roth@3ddcf219-3c89-4d16-bbf6-790a9aa18b9b>
2017-01-16 15:40:01 +0000

    added missing coco
    git-svn-id: https://sselab.de/lab2/private/svn/MontiCore/trunk/mclang/cd4analysis@26229 3ddcf219-3c89-4d16-bbf6-790a9aa18b9b

---
+++ src/main/java/de/monticore/umlcd4a/cocos/mcg2ebnf/EnumConstantNameUpperCase.java
@@ -0,0 +1,35 @@
+/*
+ * Copyright (c) 2015 RWTH Aachen. All rights reserved.
+ *
+ * http://www.se-rwth.de/
+ */
+package de.monticore.umlcd4a.cocos.mcg2ebnf;
+
+import com.google.common.base.CharMatcher;
+
+import de.monticore.umlcd4a.cd4analysis._ast.ASTCDEnum;
+import de.monticore.umlcd4a.cd4analysis._ast.ASTCDEnumConstant;
+import de.monticore.umlcd4a.cd4analysis._cocos.CD4AnalysisASTCDEnumCoCo;
+import de.se_rwth.commons.logging.Log;
+
+/**
+ * Enums may not have modifiers.
+ *
+ * @author Robert Heim
+ */
+public class EnumConstantNameUpperCase implements CD4AnalysisASTCDEnumCoCo {
+  
+  /**
+   * @see de.monticore.umlcd4a._cocos.CD4AnalysisASTCDEnumCoCo#check(de.monticore.umlcd4a.cd4analysis._ast.ASTCDEnum)
+   */
+  @Override
+  public void check(ASTCDEnum node) {
+    for (ASTCDEnumConstant a: node.getCDEnumConstants()){
+	  if (!CharMatcher.JAVA_UPPER_CASE.matchesAllOf(a.toString().replaceAll("_",""))) {
+        Log.error(String.format("0xC4A73 Enum %s may only have lower case constants.", node.getName()),
+          a.get_SourcePositionStart());
+        break;
+	  }  
+	}      
+  }
+}
