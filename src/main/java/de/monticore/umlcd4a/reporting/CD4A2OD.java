d93bc646f994bbed469fb1fb6a514afd3a61d93a
breuer <breuer@3ddcf219-3c89-4d16-bbf6-790a9aa18b9b>
2017-06-06 13:51:25 +0000

    Adapt report visitor (version 1.3.10)
    git-svn-id: https://sselab.de/lab2/private/svn/MontiCore/trunk/mclang/cd4analysis@26437 3ddcf219-3c89-4d16-bbf6-790a9aa18b9b

--- src/main/java/de/monticore/umlcd4a/reporting/CD4A2OD.java
+++ src/main/java/de/monticore/umlcd4a/reporting/CD4A2OD.java
@@ -10,7 +10,6 @@ import de.monticore.literals.literals._od.Literals2OD;
 import de.monticore.prettyprint.IndentPrinter;
 import de.monticore.types.types._od.Types2OD;
 import de.monticore.umlcd4a.cd4analysis._od.CD4Analysis2OD;
-import de.monticore.umlcd4a.cd4analysis._visitor.CD4AnalysisDelegatorVisitor;
 import de.monticore.umlcd4a.cd4analysis._visitor.CD4AnalysisVisitor;
 import de.monticore.umlcd4a.cd4analysis._visitor.CommonCD4AnalysisDelegatorVisitor;
 
@@ -19,14 +18,14 @@ public class CD4A2OD extends CD4Analysis2OD {
     
   private CD4AnalysisVisitor realThis = this;
   
-  private final CD4AnalysisDelegatorVisitor visitor;
+  private final CommonCD4AnalysisDelegatorVisitor visitor;
   
   public CD4A2OD(IndentPrinter printer, ReportingRepository reporting) {
     super(printer, reporting);
     visitor = new CommonCD4AnalysisDelegatorVisitor();
     visitor.set_de_monticore_literals_literals__visitor_LiteralsVisitor(new Literals2OD(printer, reporting));
     visitor.set_de_monticore_types_types__visitor_TypesVisitor(new Types2OD(printer, reporting));
-    visitor.set_de_monticore_umlcd4a_cd4analysis__visitor_CD4AnalysisVisitor(this);  }
+    visitor.set_de_monticore_umlcd4a_cd4analysis__visitor_CD4AnalysisVisitor(new CD4Analysis2OD(printer, reporting));  }
 
   
   /**
