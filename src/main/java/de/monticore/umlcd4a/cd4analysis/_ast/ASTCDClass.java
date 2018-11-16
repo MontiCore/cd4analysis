7008ef5ba762f39c01c1107ab3861f9aeb5b3f9f
Michael von Wenckstern <vonwenckstern@se-rwth.de>
2018-11-16 22:22:37 +0100

    changed grammar to use TImplements indirect token instead of "implements" to have `implements` as role name
    TImplements = { _input.LT(1).getText().equals("implements") }? Name;

--- src/main/java/de/monticore/umlcd4a/cd4analysis/_ast/ASTCDClass.java
+++ src/main/java/de/monticore/umlcd4a/cd4analysis/_ast/ASTCDClass.java
@@ -38,11 +38,12 @@ public class ASTCDClass extends ASTCDClassTOP {
       Optional<ASTModifier> modifier,
       String name,
       Optional<ASTReferenceType> superclass,
+      Optional<ASTTImplements> r__implements,
       java.util.List<de.monticore.types.types._ast.ASTReferenceType> interfaces,
       java.util.List<de.monticore.umlcd4a.cd4analysis._ast.ASTCDAttribute> cDAttributes,
       java.util.List<de.monticore.umlcd4a.cd4analysis._ast.ASTCDConstructor> cDConstructors,
       java.util.List<de.monticore.umlcd4a.cd4analysis._ast.ASTCDMethod> cDMethods) {
-    super(modifier, name, superclass, interfaces, cDAttributes,
+    super(modifier, name, superclass, r__implements, interfaces, cDAttributes,
         cDConstructors, cDMethods);
   }
   
