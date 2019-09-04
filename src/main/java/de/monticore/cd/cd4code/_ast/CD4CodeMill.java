dc0f39fb410d9c667746f1a32470e2f6e8605961
pichler <pichler@se-rwth.de>
2019-09-04 10:00:06 +0200

    create new bugfix branch 1.3.20.1 with fixed mill

---
+++ src/main/java/de/monticore/cd/cd4code/_ast/CD4CodeMill.java
@@ -0,0 +1,37 @@
+package de.monticore.cd.cd4code._ast;
+
+import de.monticore.cd.cd4analysis._ast.*;
+
+public class CD4CodeMill extends CD4CodeMillTOP {
+  public static ASTCDAssociationBuilder cDAssociationBuilder() {
+    return CD4AnalysisMill.cDAssociationBuilder();
+  }
+
+  public static ASTCDAttributeBuilder cDAttributeBuilder() {
+    return CD4AnalysisMill.cDAttributeBuilder();
+  }
+
+  public static ASTCDClassBuilder cDClassBuilder() {
+    return CD4AnalysisMill.cDClassBuilder();
+  }
+
+  public static ASTCDConstructorBuilder cDConstructorBuilder() {
+    return CD4AnalysisMill.cDConstructorBuilder();
+  }
+
+  public static ASTCDEnumBuilder cDEnumBuilder() {
+    return CD4AnalysisMill.cDEnumBuilder();
+  }
+
+  public static ASTCDInterfaceBuilder cDInterfaceBuilder() {
+    return CD4AnalysisMill.cDInterfaceBuilder();
+  }
+
+  public static ASTCDMethodBuilder cDMethodBuilder() {
+    return CD4AnalysisMill.cDMethodBuilder();
+  }
+
+  public static ASTCDQualifierBuilder cDQualifierBuilder() {
+    return CD4AnalysisMill.cDQualifierBuilder();
+  }
+}
