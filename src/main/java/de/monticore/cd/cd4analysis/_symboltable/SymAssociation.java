d15a55270a98f5d6c6612395f566e50c485b9e92
Simon Varga <varga@se-rwth.de>
2020-03-16 15:30:23 +0100

    new version of CD4Analysis and CD4Code

---
+++ src/main/java/de/monticore/cd/cd4analysis/_symboltable/SymAssociation.java
@@ -0,0 +1,18 @@
+package de.monticore.cd.cd4analysis._symboltable;
+
+import java.util.Optional;
+
+public class SymAssociation {
+  protected Optional<CDAssociationSymbol> association;
+  protected CDRoleSymbol left, right;
+
+  public SymAssociation(Optional<CDAssociationSymbol> association, CDRoleSymbol left, CDRoleSymbol right) {
+    this.association = association;
+    this.left = left;
+    this.right = right;
+  }
+
+  public SymAssociation(CDRoleSymbol left, CDRoleSymbol right) {
+    this(Optional.empty(), left, right);
+  }
+}
---
+++ src/main/java/de/monticore/cd/cd4analysis/_symboltable/SymModifier.java
@@ -0,0 +1,19 @@
+package de.monticore.cd.cd4analysis._symboltable;
+
+public class SymModifier {
+  private boolean isAbstract;
+  private boolean isFinal;
+  private boolean isStatic;
+
+  public boolean isAbstract() {
+    return isAbstract;
+  }
+
+  public boolean isFinal() {
+    return isFinal;
+  }
+
+  public boolean isStatic() {
+    return isStatic;
+  }
+}
