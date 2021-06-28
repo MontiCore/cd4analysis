ba6481e4ee3d1ed719b3e893f943d1ec554fb8ad
breuer <breuer@se-rwth.de>
2021-06-28 11:44:01 +0200

    don't override checkifContinueAsSubscope

--- src/main/java/de/monticore/cdbasis/_symboltable/CDBasisArtifactScope.java
+++ src/main/java/de/monticore/cdbasis/_symboltable/CDBasisArtifactScope.java
@@ -21,18 +21,4 @@ public class CDBasisArtifactScope extends CDBasisArtifactScopeTOP {
     super(enclosingScope, packageName, imports);
   }
 
-  @Override
-  public boolean checkIfContinueAsSubScope(String symbolName) {
-    return true;
-
-    /*
-      always check the subscopes
-      there are 2 constellations, what the symbolName could contain:
-      1. an absolute name like "a.b.A":
-         in this case, we traverse further in the subscopes
-         (possibly with the package name removed, when this.getName() has parts of the package name)
-      2. a QualifiedName without a package, like "A" or "A.name":
-         search in all subscopes, for any defined type with this name
-     */
-  }
 }
