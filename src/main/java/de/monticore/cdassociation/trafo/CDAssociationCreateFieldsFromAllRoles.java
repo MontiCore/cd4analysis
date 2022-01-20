511651f906b6fb7bf036a63faacebf8ba5d627ae
Marita <breuer@se-rwth.de>
2022-01-20 12:41:09 +0100

    create variables(AST) for associations

--- src/main/java/de/monticore/cdassociation/trafo/CDAssociationCreateFieldsFromAllRoles.java
+++ src/main/java/de/monticore/cdassociation/trafo/CDAssociationCreateFieldsFromAllRoles.java
@@ -1,20 +1,26 @@
 /* (c) https://github.com/MontiCore/monticore */
 package de.monticore.cdassociation.trafo;
 
+import de.monticore.ast.ASTNode;
 import de.monticore.cd.facade.MCQualifiedNameFacade;
+import de.monticore.cdassociation.CDAssociationMill;
 import de.monticore.cdassociation._ast.ASTCDCardinality;
 import de.monticore.cdassociation._ast.ASTCDRole;
 import de.monticore.cdassociation._symboltable.CDRoleSymbol;
 import de.monticore.cdassociation._visitor.CDAssociationHandler;
 import de.monticore.cdassociation._visitor.CDAssociationTraverser;
 import de.monticore.cdassociation._visitor.CDAssociationVisitor2;
+import de.monticore.cdbasis._ast.ASTCDAttribute;
+import de.monticore.cdbasis._ast.ASTCDClass;
 import de.monticore.cdbasis._ast.ASTCDCompilationUnit;
 import de.monticore.symbols.oosymbols._symboltable.FieldSymbol;
 import de.monticore.symbols.oosymbols._symboltable.FieldSymbolBuilder;
+import de.monticore.types.MCTypeFacade;
 import de.monticore.types.check.SymTypeExpression;
 import de.monticore.types.check.SymTypeExpressionFactory;
 import de.monticore.types.mcbasictypes._ast.ASTMCImportStatement;
 import de.monticore.types.mcbasictypes._ast.ASTMCQualifiedName;
+import de.monticore.umlmodifier._ast.ASTModifier;
 import de.se_rwth.commons.SourcePosition;
 import de.se_rwth.commons.logging.Log;
 
@@ -25,7 +31,7 @@ import java.util.stream.Collectors;
 import java.util.stream.Stream;
 
 public class CDAssociationCreateFieldsFromAllRoles
-    implements CDAssociationVisitor2, CDAssociationHandler {
+  implements CDAssociationVisitor2, CDAssociationHandler {
   protected CDAssociationTraverser traverser;
   protected List<ASTMCImportStatement> imports;
   protected ASTMCQualifiedName packageDeclaration;
@@ -45,43 +51,70 @@ public class CDAssociationCreateFieldsFromAllRoles
   public void visit(ASTCDRole node) {
     final Stream<FieldSymbol> stream = node.getSymbol().getEnclosingScope().getLocalFieldSymbols().stream();
     String pos = stream
-        .filter(f -> f.getName().equals(node.getName()))
-        .map(f -> {
-          if (f.getSourcePosition().equals(SourcePosition.getDefaultSourcePosition())) {
-            // if it is just a Symbol, then it might be created in the createdFields list
-            if (createdFields.containsKey(f)) {
-              return createdFields.get(f).toString();
-            }
-            return "unknown source";
+      .filter(f -> f.getName().equals(node.getName()))
+      .map(f -> {
+        if (f.getSourcePosition().equals(SourcePosition.getDefaultSourcePosition())) {
+          // if it is just a Symbol, then it might be created in the createdFields list
+          if (createdFields.containsKey(f)) {
+            return createdFields.get(f).toString();
           }
-          else {
-            return f.getSourcePosition().toString();
-          }
-        })
-        .collect(Collectors.joining(", "));
+          return "unknown source";
+        }
+        else {
+          return f.getSourcePosition().toString();
+        }
+      })
+      .collect(Collectors.joining(", "));
     if (!pos.isEmpty()) {
       final String msg = String.format("0xCD0B7: a FieldSymbol with the name '%s' already exists in '%s' (defined in %s)",
-          node.getName(), node.getSymbol().getAssoc().getOtherRole(node.getSymbol()).getType().getTypeInfo().getFullName(), pos);
+        node.getName(), node.getSymbol().getAssoc().getOtherRole(node.getSymbol()).getType().getTypeInfo().getFullName(), pos);
       Log.error(msg, node.get_SourcePositionStart(), node.get_SourcePositionEnd());
     }
 
     // create a FieldSymbol in the same scope as the RoleSymbol
     final CDRoleSymbol symbol = node.getSymbol();
     final FieldSymbol fieldSymbol = new FieldSymbolBuilder()
-        .setName(node.getName())
-        .setType(calculateType(symbol))
-        .setIsReadOnly(symbol.isIsReadOnly())
-        .setIsPrivate(symbol.isIsPrivate())
-        .setIsProtected(symbol.isIsProtected())
-        .setIsPublic(symbol.isIsPublic())
-        .setIsStatic(symbol.isIsStatic())
-        .setIsFinal(symbol.isIsFinal())
-        .build();
-
+      .setName(node.getName())
+      .setType(calculateType(symbol))
+      .setIsReadOnly(symbol.isIsReadOnly())
+      .setIsPrivate(symbol.isIsPrivate())
+      .setIsProtected(symbol.isIsProtected())
+      .setIsPublic(symbol.isIsPublic())
+      .setIsStatic(symbol.isIsStatic())
+      .setIsFinal(symbol.isIsFinal())
+      .build();
     createdFields.put(fieldSymbol, node.get_SourcePositionStart());
 
+    // add new symbol
     node.getSymbol().getEnclosingScope().add(fieldSymbol);
-    node.getSymbol().setField(fieldSymbol);
+
+    // Create the corresponding ASTNode
+    ASTModifier modifier = CDAssociationMill.modifierBuilder()
+      .setReadonly(symbol.isIsReadOnly())
+      .setPrivate(symbol.isIsPrivate())
+      .setProtected(symbol.isIsProtected())
+      .setPublic(symbol.isIsPublic())
+      .setStatic(symbol.isIsStatic())
+      .setFinal(symbol.isIsFinal())
+      .build();
+    modifier.setEnclosingScope(node.getEnclosingScope());
+
+    ASTCDAttribute fieldAst = CDAssociationMill.cDAttributeBuilder()
+      .setName(node.getName())
+      .setMCType(MCTypeFacade.getInstance().createQualifiedType(fieldSymbol.getFullName()))
+      .setModifier(modifier)
+      .build();
+    if (node.getEnclosingScope().isPresentSpanningSymbol() && node.getEnclosingScope().getSpanningSymbol().isPresentAstNode()) {
+      ASTNode spannedType = node.getEnclosingScope().getSpanningSymbol().getAstNode();
+      if (spannedType instanceof ASTCDClass) {
+        ((ASTCDClass) spannedType).addCDMember(fieldAst);
+      }
+    }
+    fieldAst.setEnclosingScope(node.getEnclosingScope());
+    fieldAst.getMCType().setEnclosingScope(node.getEnclosingScope());
+
+    fieldAst.setSymbol(fieldSymbol);
+    fieldSymbol.setAstNode(fieldAst);
   }
 
   public SymTypeExpression calculateType(CDRoleSymbol symbol) {
@@ -110,7 +143,7 @@ public class CDAssociationCreateFieldsFromAllRoles
   }
 
   public void transform(ASTCDCompilationUnit compilationUnit)
-      throws RuntimeException {
+    throws RuntimeException {
     if (!compilationUnit.getCDDefinition().isPresentSymbol()) {
       final String msg = "0xCD0B8: can't start the transformation, the symbol table is missing";
       Log.error(msg);
