e0fa1afc15c050785d775189facee4c9d99648d6
boecker <boecker@se-rwth.de>
2022-02-23 14:19:52 +0100

    add cdbasis coco tests

---
+++ src/test/java/de/monticore/testcd4codebasis/cocos/CD4CodeEnumConstantParameterMatchConstructorArgumentsTest.java
@@ -0,0 +1,45 @@
+/* (c) https://github.com/MontiCore/monticore */
+package de.monticore.testcd4codebasis.cocos;
+
+import de.monticore.cd4codebasis.cocos.ebnf.CD4CodeEnumConstantParameterMatchConstructorArguments;
+import de.monticore.cdbasis._ast.ASTCDCompilationUnit;
+import de.monticore.testcd4codebasis.CD4CodeBasisTestBasis;
+import de.se_rwth.commons.logging.Log;
+import org.junit.After;
+import org.junit.Test;
+
+import java.io.IOException;
+import java.util.Optional;
+
+import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.assertTrue;
+
+public class CD4CodeEnumConstantParameterMatchConstructorArgumentsTest extends CD4CodeBasisTestBasis {
+
+  @Test
+  public void testValid() throws IOException {
+    coCoChecker.addCoCo(new CD4CodeEnumConstantParameterMatchConstructorArguments());
+    final Optional<ASTCDCompilationUnit> optAST = p.parse(getFilePath("cd4codebasis/cocos/CD4CodeEnumConstantParameterMatchConstructorArgumentsValid.cd"));
+    assertTrue(optAST.isPresent());
+    final ASTCDCompilationUnit ast = optAST.get();
+    Log.getFindings().clear();
+    coCoChecker.checkAll(ast);
+    assertTrue(Log.getFindings().isEmpty());
+  }
+
+  @Test
+  public void testInvalid() throws IOException {
+    coCoChecker.addCoCo(new CD4CodeEnumConstantParameterMatchConstructorArguments());
+    final Optional<ASTCDCompilationUnit> optAST = p.parse(getFilePath("cd4codebasis/cocos/CD4CodeEnumConstantParameterMatchConstructorArgumentsInvalid.cd"));
+    assertTrue(optAST.isPresent());
+    final ASTCDCompilationUnit ast = optAST.get();
+    Log.getFindings().clear();
+    coCoChecker.checkAll(ast);
+    assertEquals(1, Log.getFindings().size());
+    assertTrue(Log.getFindings().get(0).getMsg().startsWith("0xCDC90"));
+  }
+
+  @After
+  public void after(){}
+
+}
