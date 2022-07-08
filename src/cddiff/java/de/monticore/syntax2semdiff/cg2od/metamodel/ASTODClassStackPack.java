5051642082cf8253a9cea7d8077786f96988dc46
Max Philip Stachon <max.stachon@rwth-aachen.de>
2022-07-08 16:14:08 +0200

    rename sydiff2semdiff to syntax2semdiff

---
+++ src/cddiff/java/de/monticore/syntax2semdiff/cg2od/metamodel/ASTODClassStackPack.java
@@ -0,0 +1,32 @@
+package de.monticore.syntax2semdiff.cg2od.metamodel;
+
+import de.monticore.odbasis._ast.ASTODNamedObject;
+import de.monticore.syntax2semdiff.cd2sg.metamodel.SupportClass;
+
+import java.util.List;
+
+public class ASTODClassStackPack {
+  private List<ASTODNamedObject> namedObjects;
+  private SupportClass supportClass;
+
+  public ASTODClassStackPack(List<ASTODNamedObject> namedObjects, SupportClass supportClass) {
+    this.namedObjects = namedObjects;
+    this.supportClass = supportClass;
+  }
+
+  public List<ASTODNamedObject> getNamedObjects() {
+    return namedObjects;
+  }
+
+  public void setNamedObjects(List<ASTODNamedObject> namedObjects) {
+    this.namedObjects = namedObjects;
+  }
+
+  public SupportClass getSupportClass() {
+    return supportClass;
+  }
+
+  public void setSupportClass(SupportClass supportClass) {
+    this.supportClass = supportClass;
+  }
+}
