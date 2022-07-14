3f299b699acea43e0f6013cf971385664d81d0b1
niklasdienstknecht <dienstknecht@se-rwth.de>
2022-07-14 12:54:27 +0200

    use OOClass2MCResolver

--- src/main/java/de/monticore/cd/_symboltable/BuiltInTypes.java
+++ src/main/java/de/monticore/cd/_symboltable/BuiltInTypes.java
@@ -3,7 +3,7 @@ package de.monticore.cd._symboltable;
 
 import de.monticore.cd4analysis.CD4AnalysisMill;
 import de.monticore.cd4analysis._symboltable.ICD4AnalysisScope;
-import de.monticore.class2mc.Class2MCResolver;
+import de.monticore.class2mc.OOClass2MCResolver;
 import de.monticore.symbols.basicsymbols.BasicSymbolsMill;
 import de.monticore.symbols.oosymbols._symboltable.IOOSymbolsGlobalScope;
 import de.monticore.symbols.oosymbols._symboltable.OOTypeSymbol;
@@ -24,7 +24,7 @@ public class BuiltInTypes {
       setUpCD4AType("Set", "T");
       setUpCD4AType("Map", "K", "V");
 
-      final Class2MCResolver resolver = new Class2MCResolver();
+      final OOClass2MCResolver resolver = new OOClass2MCResolver();
       globalScope.addAdaptedTypeSymbolResolver(
           (boolean foundSymbols, String name, de.monticore.symboltable.modifiers.AccessModifier modifier, java.util.function.Predicate<de.monticore.symbols.basicsymbols._symboltable.TypeSymbol> predicate) ->
               new ArrayList<>(resolver.resolveAdaptedOOTypeSymbol(foundSymbols, name, modifier, predicate::test)));
