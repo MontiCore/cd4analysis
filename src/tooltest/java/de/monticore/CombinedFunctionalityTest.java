df2a8d7d36107009399f1ec5f76033f197f94851
Sebastian Stüber <stueber@se-rwth.de>
2023-02-02 08:50:17 +0100

    setup for MaCoCo merge

--- src/tooltest/java/de/monticore/CombinedFunctionalityTest.java
+++ src/tooltest/java/de/monticore/CombinedFunctionalityTest.java
@@ -7,13 +7,21 @@ import de.monticore.cdbasis._ast.ASTCDCompilationUnit;
 import de.monticore.cddiff.CDDiff;
 import de.monticore.cddiff.alloycddiff.CDSemantics;
 import de.monticore.cdmerge.CDMerge;
+
+import java.io.File;
 import java.io.IOException;
+import java.util.Arrays;
 import java.util.HashSet;
 import java.util.Optional;
 import java.util.Set;
+import java.util.stream.Collectors;
+
 import org.junit.Assert;
 import org.junit.Before;
 import org.junit.Test;
+
+import static org.junit.jupiter.api.Assertions.assertTrue;
+import static org.junit.jupiter.api.Assertions.fail;
 
 public class CombinedFunctionalityTest {
 
@@ -27,27 +35,35 @@ public class CombinedFunctionalityTest {
 
   @Test
   public void testMaCoCo() {
+    String base_path = "src/test/resources/de/monticore/cd4analysis/examples/industrial_strength_models/";
     try {
       CD4CodeParser parser = CD4CodeMill.parser();
+
+      Set<ASTCDCompilationUnit> mergeSet =
+        Arrays.stream(new File(base_path + "MaCoCoMerge/").listFiles())
+          .map(file -> {
+            try {
+              return parser.parse(file.getAbsolutePath());
+            } catch (IOException e) {
+              fail();
+              return null;
+            }
+          })
+          .peek(optional -> assertTrue(optional.isPresent()))
+          .map(Optional::get)
+          .collect(Collectors.toUnmodifiableSet());
+
+      ASTCDCompilationUnit merged = CDMerge.merge(mergeSet, "MergedDomain", new HashSet<>());
+
+
       Optional<ASTCDCompilationUnit> expected =
-          parser.parse(
-              "src/test/resources/de"
-                  + "/monticore"
-                  + "/cd4analysis"
-                  + "/examples"
-                  + "/industrial_strength_models/MaCoCo.cd");
+        parser.parse(base_path + "MaCoCo.cd");
       Assert.assertTrue(expected.isPresent());
 
-      // todo: merge parts of MaCoCo
-      Set<ASTCDCompilationUnit> mergeSet = new HashSet<>();
-      mergeSet.add(expected.get().deepClone());
-      mergeSet.add(expected.get().deepClone());
-
-      ASTCDCompilationUnit result = CDMerge.merge(mergeSet, "MergedDomain", new HashSet<>());
 
       Assert.assertTrue(
           CDDiff.computeSyntax2SemDiff(
-                  result, expected.get(), CDSemantics.MULTI_INSTANCE_CLOSED_WORLD)
+                  merged, expected.get(), CDSemantics.MULTI_INSTANCE_CLOSED_WORLD)
               .isEmpty());
 
     } catch (IOException e) {
