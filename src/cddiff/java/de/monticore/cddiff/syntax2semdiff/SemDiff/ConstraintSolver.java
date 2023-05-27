930218f53779301eba32b95f371085b6283683e6
Presiyan <presiyan.prodanov@rwth-aachen.de>
2023-05-27 21:59:42 +0200

    ConstraintSolver with possible implementation for new functions

---
+++ src/cddiff/java/de/monticore/cddiff/syntax2semdiff/SemDiff/ConstraintSolver.java
@@ -0,0 +1,313 @@
+package de.monticore.cddiff.syntax2semdiff.SemDiff;
+
+import com.google.common.collect.ArrayListMultimap;
+import de.monticore.cdassociation._ast.ASTCDAssociation;
+import de.monticore.cdbasis._ast.ASTCDClass;
+import de.monticore.cdbasis._ast.ASTCDCompilationUnit;
+import de.monticore.cdbasis._symboltable.CDTypeSymbol;
+
+import java.util.ArrayList;
+import java.util.List;
+
+import static de.monticore.cddiff.ow2cw.CDAssociationHelper.matchRoleNames;
+
+/*
+ * A function to transform association into matrix must be implemented.
+ * A function with a suitable datastructure for getting the values from the matrix needs to be implemented.
+ * */
+public class ConstraintSolver {
+/*
+  private ASTCDAssociation mergeAssociations(List<ASTCDAssociation> astcdAssociationList, ASTCDCompilationUnit astcdCompilationUnit) {
+    List<ASTCDAssociation> assocsToMerge = new ArrayList<>();
+    for (ASTCDAssociation astcdAssociation : astcdAssociationList) {
+      for (ASTCDAssociation astcdAssociation1 : astcdAssociationList) {
+        if (!astcdAssociation.equals(astcdAssociation1) & matchRoleNames(astcdAssociation.getLeft(), astcdAssociation1.getLeft()) & matchRoleNames(astcdAssociation
+          .getRight(), astcdAssociation1.getRight()) & astcdCompilationUnit.getEnclosingScope().resolveDiagramDown(astcdAssociation.getLeftQualifiedName().getQName()).equals(astcdCompilationUnit.getEnclosingScope().resolveDiagramDown(astcdAssociation1.getLeftQualifiedName().getQName()))) {
+
+        }
+      }
+    }
+  }
+
+  private void mergeAssociations(ASTCDCompilationUnit astcdCompilationUnit) {
+    ArrayListMultimap<ASTCDAssociation, ASTCDAssociation> map = ArrayListMultimap.create();
+    for (ASTCDAssociation astcdAssociation : astcdCompilationUnit.getCDDefinition().getCDAssociationsList()) {
+      map.put(astcdAssociation, null);
+      for (ASTCDAssociation astcdAssociation1 : astcdCompilationUnit.getCDDefinition().getCDAssociationsList()) {
+        if (!astcdAssociation.equals(astcdAssociation1) && matchRoleNames(astcdAssociation.getLeft(), astcdAssociation1.getLeft()) && matchRoleNames(astcdAssociation
+          .getRight(), astcdAssociation1.getRight()) &&
+          astcdCompilationUnit.getEnclosingScope().resolveDiagramDown(astcdAssociation.getLeftQualifiedName().getQName()).equals(astcdCompilationUnit.getEnclosingScope().resolveDiagramDown(astcdAssociation1.getLeftQualifiedName().getQName()))
+          && astcdCompilationUnit.getEnclosingScope().resolveDiagramDown(astcdAssociation.getLeftQualifiedName().getQName()).equals(astcdCompilationUnit.getEnclosingScope().resolveDiagramDown(astcdAssociation1.getLeftQualifiedName().getQName()))) {
+          map.put(astcdAssociation, astcdAssociation1);
+        }
+      }
+    }
+    for (ASTCDAssociation astcdAssociation : map.keySet()) {
+      if (astcdAssociation.getLeft().getCDCardinality().isMult() || astcdAssociation.getRight().getCDCardinality().isMult()) {
+        for (ASTCDAssociation astcdAssociation1 : map.get(astcdAssociation)) {
+          astcdAssociation1.get
+        }
+      }
+    }
+  }
+
+  private ArrayListMultimap computeRanges(ASTCDCompilationUnit astcdCompilationUnit) {
+    ArrayListMultimap arrayListMultimap = ArrayListMultimap.create();
+    for (ASTCDClass astcdClass : astcdCompilationUnit.getCDDefinition().getCDClassesList()) {
+      for (ASTCDAssociation astcdAssociation : astcdCompilationUnit.getCDDefinition().getCDAssociationsListForType(astcdClass)) {
+
+      }
+    }
+    return arrayListMultimap;
+  }
+
+
+  private static void toMatrix() {
+    List<List<Integer>> ranges = new ArrayList<>();
+    int numRows = ranges.size();
+    int numCols = ranges.stream().mapToInt(List::size).sum();
+
+    int[][] A = new int[numRows][numCols];
+    int[][] B = new int[numRows][numCols];
+    int[] a = new int[numRows];
+    int[] b = new int[numRows];
+
+    int colIndex = 0;
+    for (int i = 0; i < numRows; i++) {
+      List<Integer> range = ranges.get(i);
+      int lowerRange = range.get(0);
+      int upperRange = range.get(1);
+
+      for (int j = colIndex; j < colIndex + range.size(); j++) {
+        A[i][j] = 1;
+        B[i][j] = 1;
+      }
+
+      a[i] = lowerRange;
+      b[i] = upperRange;
+
+      colIndex += range.size();
+    }
+  }
+*/
+  /*
+   * Transform a system of inequalities Ax<=b
+   * to a system of equalities Ax=b.
+   * We use slack variables that represent the difference between both types.
+   * The function returns a matrix with its vector [B|c]
+   * */
+  private static int[][] transformInequalitiesToEqualities(int[][] A, int[][] B, int[] a, int[] b) {
+    int numRowsA = A.length;
+    int numColsA = A[0].length;
+    int numRowsB = B.length;
+    int numColsB = B[0].length;
+
+    int numSlackVariables = numRowsA + numRowsB;
+    int numVariables = numColsA + numColsB + numSlackVariables;
+
+    int[][] equalities = new int[numRowsA + numRowsB][numVariables + 1];
+
+    for (int i = 0; i < numRowsA; i++) {
+      System.arraycopy(A[i], 0, equalities[i], 0, numColsA);
+      equalities[i][numColsA + i] = 1;  // Slack variable
+      equalities[i][numVariables] = a[i];
+    }
+
+    for (int i = 0; i < numRowsB; i++) {
+      System.arraycopy(B[i], 0, equalities[numRowsA + i], numColsA, numColsB);
+      equalities[numRowsA + i][numColsA + numSlackVariables + i] = 1;  // Slack variable
+      equalities[numRowsA + i][numVariables] = b[i];
+    }
+
+    return equalities;
+  }
+  /*
+  private static int[][] toEqualityM(int[][] A, int[] b){
+    int m = A.length;
+    int n = A[0].length;
+
+    // create a new matrix with slack variables
+    int[][] B = new int[m][n + m];
+    for (int i = 0; i < m; i++) {
+      for (int j = 0; j < n; j++) {
+        B[i][j] = A[i][j];
+      }
+      B[i][n + i] = 1;
+    }
+
+    // create a new right-hand side vector with zeros and slack variables
+    int[] c = new int[n + m];
+    for (int i = 0; i < n; i++) {
+      c[i] = 0;
+    }
+    for (int i = 0; i < m; i++) {
+      c[n + i] = b[i];
+    }
+
+    int[][] solution = new int[B.length][B[0].length+1];
+    for (int i = 0; i < B.length; i++) {
+      for (int j = 0; j < B[0].length; j++) {
+        solution[i][j] = B[i][j];
+      }
+      solution[i][B[0].length] = b[i];
+    }
+
+    return solution;
+  }
+    private static int[][] toEqualityM(int[][] A, int[] b, int[][] B, int[] c) {
+    int numRowsA = A.length;
+    int numColsA = A[0].length;
+    int numRowsB = B.length;
+    int numColsB = B[0].length;
+
+    // create a new matrix with slack and surplus variables
+    int[][] C = new int[numRowsA + numRowsB][numColsA + numColsB];
+    for (int i = 0; i < numRowsA; i++) {
+        for (int j = 0; j < numColsA; j++) {
+            C[i][j] = A[i][j];
+        }
+        for (int j = 0; j < numColsB; j++) {
+            C[i][numColsA + j] = 0; // Slack variables
+        }
+    }
+    for (int i = 0; i < numRowsB; i++) {
+        for (int j = 0; j < numColsA; j++) {
+            C[numRowsA + i][j] = 0; // Surplus variables
+        }
+        for (int j = 0; j < numColsB; j++) {
+            C[numRowsA + i][numColsA + j] = -B[i][j]; // Surplus variables with negation
+        }
+    }
+
+    // create a new right-hand side vector with slack and surplus variables
+    int[] v = new int[numRowsA + numRowsB];
+    for (int i = 0; i < numRowsA; i++) {
+        v[i] = b[i];
+    }
+    for (int i = 0; i < numRowsB; i++) {
+        v[numRowsA + i] = -c[i]; // Surplus variables with negation
+    }
+
+    int[][] solution = new int[C.length][C[0].length + 1];
+    for (int i = 0; i < C.length; i++) {
+        for (int j = 0; j < C[0].length; j++) {
+            solution[i][j] = C[i][j];
+        }
+        solution[i][C[0].length] = v[i];
+    }
+
+    return solution;
+}
+    */
+
+  /*
+   * Gaussian elimination for Ax=b with partial pivoting
+   * where A is a matrix and b is a vector.
+   * http://www.math.sjsu.edu/~foster/m143m/gaussian_elimination_algorithms_4.pdf
+   */
+  private static int[] solve(int[][] A, int[] b) {
+    int pivotRow = -1;
+    int pivotCol = -1;
+    int[] solution = new int[A[0].length];
+    boolean[] used = new boolean[A[0].length];//a pivot element can be used only once
+
+    for (int i = 0; i < A.length; i++) {
+      for (int j = 0; j < A[0].length; j++) {
+        //find the biggest element in the row
+        if (!used[j] && A[i][j] > 0 && (pivotCol == -1 || A[i][j] * A[pivotRow][pivotCol] > A[pivotRow][j] * A[i][pivotCol])) {
+          pivotRow = i;
+          pivotCol = j;
+        }
+      }
+
+      //No pivot element found - no solution
+      if (pivotCol == -1) {
+        throw new RuntimeException();
+      }
+
+      used[pivotCol] = true;
+
+      int pivotValue = A[pivotRow][pivotCol];
+      for (int j = 0; j < A[0].length; j++) {
+        if (!used[j]) {
+          int factor = A[j][pivotCol] / pivotValue;//number for each underlying element in the pivot row
+          for (int k = 0; k < A.length; k++) {
+            A[j][k] = A[j][k] - factor * A[pivotRow][k];
+          }
+          b[j] = b[j] - factor * b[pivotRow];
+        }
+      }
+
+      solution[pivotCol] = b[pivotRow] / pivotValue;
+    }
+
+    return solution;
+  }
+
+  /* Computes the inverse of a square matrix with non-negative integers using Gaussian elimination
+   * https://code.bcanotesnepal.com/2021/04/28/matrix-inverse-using-gauss-jordan-method-c-program/
+   * */
+  private static int[][] inverse(int[][] matrix) {
+    int n = matrix.length;
+    int[][] inverse = new int[n][n];
+    int[][] augmented = new int[n][2 * n];
+
+    for (int i = 0; i < n; i++) {
+      for (int j = 0; j < n; j++) {
+        augmented[i][j] = matrix[i][j];
+        if (i == j) {
+          augmented[i][j + n] = 1;//The right side is an identity matrix with the same size of A
+        }
+      }
+    }
+
+    // Gaussian algorithm
+    for (int i = 0; i < n; i++) {
+
+      int maxRow = i;
+      //Find the biggest element in the column
+      for (int j = i + 1; j < n; j++) {
+        if (augmented[j][i] > augmented[maxRow][i]) {
+          maxRow = j;
+        }
+      }
+
+      //Can't inverse matrix
+      if (augmented[maxRow][i] == 0) {
+        throw new RuntimeException();
+      }
+
+      // Swap pivot row with current row
+      if (maxRow != i) {
+        int[] temp = augmented[i];
+        augmented[i] = augmented[maxRow];
+        augmented[maxRow] = temp;
+      }
+
+      // Scale current row to have 1 in pivot position
+      int pivot = augmented[i][i];
+      for (int j = i; j < 2 * n; j++) {
+        augmented[i][j] = augmented[i][j] / pivot;
+      }
+
+      // Subtract current row from all other rows
+      for (int j = 0; j < n; j++) {
+        if (j != i) {
+          int factor = augmented[j][i];
+          for (int k = i; k < 2 * n; k++) {
+            augmented[j][k] = augmented[j][k] - factor * augmented[i][k];
+          }
+        }
+      }
+    }
+
+    for (int i = 0; i < n; i++) {
+      for (int j = 0; j < n; j++) {
+        inverse[i][j] = augmented[i][j + n];//Inverse matrix on the right side
+      }
+    }
+
+    return inverse;
+  }
+
+}
