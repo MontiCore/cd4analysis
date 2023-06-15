5cab53f2dd2078d05384cdc75e0c78f0a6439047
Presiyan <presiyan.prodanov@rwth-aachen.de>
2023-06-15 16:37:08 +0200

    Remove test

--- src/tooltest/java/de/monticore/CDDiffCLIToolTest.java
+++ src/tooltest/java/de/monticore/CDDiffCLIToolTest.java
@@ -1,26 +1,24 @@
 /* (c) https://github.com/MontiCore/monticore */
 package de.monticore;
 
-import static org.junit.Assert.*;
-
-import de.monticore.cd._symboltable.BuiltInTypes;
-import de.monticore.cd4code.CD4CodeMill;
 import de.monticore.cdbasis._ast.ASTCDCompilationUnit;
 import de.monticore.cddiff.CDDiffUtil;
 import de.monticore.cddiff.alloycddiff.CDSemantics;
 import de.monticore.odvalidity.OD2CDMatcher;
 import de.se_rwth.commons.logging.Log;
-import java.io.File;
-import java.io.IOException;
-import java.nio.file.Path;
-import java.nio.file.Paths;
-import java.util.LinkedList;
-import java.util.List;
-import java.util.Objects;
 import org.apache.commons.io.file.PathUtils;
 import org.junit.Assert;
 import org.junit.Before;
 import org.junit.Test;
+
+import java.io.File;
+import java.io.IOException;
+import java.nio.file.Paths;
+import java.util.LinkedList;
+import java.util.List;
+import java.util.Objects;
+
+import static org.junit.Assert.*;
 
 public class CDDiffCLIToolTest {
 
@@ -570,32 +568,6 @@ public class CDDiffCLIToolTest {
       e.printStackTrace();
       Log.warn("This should not happen!");
       Assert.fail();
-    }
-  }
-
-  @Test
-  public void testMyDiff() {
-    // given 2 CDs that are not semantically equivalent
-    final String cd1 = "src/cddifftest/resources/de/monticore/cddiff/MyDiffs/CD12.cd";
-    final String cd2 = "src/cddifftest/resources/de/monticore/cddiff/MyDiffs/CD11.cd";
-
-    CD4CodeMill.init();
-    CD4CodeMill.globalScope().init();
-    BuiltInTypes.addBuiltInTypes(CD4CodeMill.globalScope());
-
-    try {
-      for (int i = 1; i <= 5; i++) {
-        Assert.assertTrue(
-            new OD2CDMatcher()
-                .checkIfDiffWitness(
-                    CDSemantics.SIMPLE_CLOSED_WORLD,
-                    Path.of(cd1).toFile(),
-                    Path.of(cd2).toFile(),
-                    Path.of("src/cddifftest/resources/de/monticore/cddiff/MyDiffs/D" + i + ".od")
-                        .toFile()));
-      }
-    } catch (NullPointerException | IOException e) {
-      fail(e.getMessage());
     }
   }
 }
