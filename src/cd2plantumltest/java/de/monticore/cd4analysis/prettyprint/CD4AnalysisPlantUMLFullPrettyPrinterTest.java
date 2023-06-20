bca6a454e0db02c8e5c7fd4fc7d91941a3cd862d
Philipp Wynands <philippwynands@gmx.de>
2023-06-20 14:55:04 +0200

    WIP: making PlantUML parsing work (Yvonne, Dan, Philipp)

--- src/cd2plantumltest/java/de/monticore/cd4analysis/prettyprint/CD4AnalysisPlantUMLFullPrettyPrinterTest.java
+++ src/cd2plantumltest/java/de/monticore/cd4analysis/prettyprint/CD4AnalysisPlantUMLFullPrettyPrinterTest.java
@@ -1,8 +1,19 @@
 package de.monticore.cd4analysis.prettyprint;
 
+import de.monticore.cd.plantuml.PlantUMLConfig;
+import de.monticore.cd.plantuml.PlantUMLUtil;
 import de.monticore.cd4analysis.CD4AnalysisTestBasis;
 import de.monticore.cd4analysis._parser.CD4AnalysisParser;
+import de.monticore.cd4analysis._prettyprint.CD4AnalysisFullPrettyPrinter;
+import de.monticore.cd4analysis.trafo.CD4AnalysisAfterParseTrafo;
+import de.monticore.cdbasis._ast.ASTCDCompilationUnit;
+import de.monticore.prettyprint.IndentPrinter;
+import net.sourceforge.plantuml.graph2.Plan;
 import org.junit.Before;
+import org.junit.Test;
+
+import java.io.IOException;
+import java.util.Optional;
 
 public class CD4AnalysisPlantUMLFullPrettyPrinterTest extends CD4AnalysisTestBasis {
 
@@ -12,8 +23,32 @@ public class CD4AnalysisPlantUMLFullPrettyPrinterTest extends CD4AnalysisTestBas
   @Before
   public void initObjects() {
 
+    // printer = new CD4AnalysisFullPrettyPrinter(new IndentPrinter());
     printer = new CD4AnalysisPlantUMLFullPrettyPrinter();
     p = new CD4AnalysisParser();
+
+  }
+
+  @Test
+  public void completeModel() throws IOException {
+
+    String filePath = getFilePath("cd4analysis/parser/Simple.cd");
+
+    final Optional<ASTCDCompilationUnit> astcdCompilationUnit =
+      p.parseCDCompilationUnit(filePath);
+
+    checkNullAndPresence(p, astcdCompilationUnit);
+    final ASTCDCompilationUnit node = astcdCompilationUnit.get();
+
+    //String outputPath = PlantUMLUtil.printCD2PlantUMLLocally(astcdCompilationUnit, "./", new PlantUMLConfig());
+    String output = printer.prettyprint(node);
+
+    // new CD4AnalysisAfterParseTrafo().transform(node);
+    // String output = printer.prettyprint(node);
+//
+    // final Optional<ASTCDCompilationUnit> astcdCompilationUnitReParsed =
+    //   p.parse_StringCDCompilationUnit(output);
+    // checkNullAndPresence(p, astcdCompilationUnitReParsed);
 
   }
 
