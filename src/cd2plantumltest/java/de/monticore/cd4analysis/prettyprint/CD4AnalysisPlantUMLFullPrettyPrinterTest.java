529a295f663e4642c9f2ed10153fcce556c2942b
Philipp Wynands <philippwynands@gmx.de>
2023-06-20 14:06:56 +0200

    ADDED: cd2plantumltest package (Philipp, Yvonne, Dan)

--- src/cd2plantumltest/java/de/monticore/cd4analysis/prettyprint/CD4AnalysisPlantUMLFullPrettyPrinterTest.java
+++ src/cd2plantumltest/java/de/monticore/cd4analysis/prettyprint/CD4AnalysisPlantUMLFullPrettyPrinterTest.java
@@ -1,32 +1,24 @@
 package de.monticore.cd4analysis.prettyprint;
 
-import de.monticore.cd.TestBasis;
-import de.monticore.cd4analysis.prettyprint.CD4AnalysisFullPrettyPrinter;
 import de.monticore.cd4analysis.CD4AnalysisTestBasis;
 import de.monticore.cd4analysis._parser.CD4AnalysisParser;
-import de.monticore.cd4analysis.trafo.CD4AnalysisAfterParseTrafo;
-import de.monticore.cdbasis._ast.ASTCDCompilationUnit;
 import org.junit.Before;
-import org.junit.Test;
-
-import java.io.IOException;
-import java.util.Optional;
 
 public class CD4AnalysisPlantUMLFullPrettyPrinterTest extends CD4AnalysisTestBasis {
 
- // protected CD4AnalysisPlantUMLFullPrettyPrinter printer;
+  protected CD4AnalysisPlantUMLFullPrettyPrinter printer;
   protected CD4AnalysisParser p;
 
   @Before
   public void initObjects() {
 
-  //  printer = new CD4AnalysisPlantUMLFullPrettyPrinter();
+    printer = new CD4AnalysisPlantUMLFullPrettyPrinter();
     p = new CD4AnalysisParser();
 
   }
 
 
- // @Test
+  // @Test
  /* public void completeModel() throws IOException {
 
     final Optional<ASTCDCompilationUnit> astcdCompilationUnit =
