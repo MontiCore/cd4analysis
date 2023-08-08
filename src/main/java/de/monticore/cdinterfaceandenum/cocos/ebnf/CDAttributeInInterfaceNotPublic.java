385fedb26825f9fab967b7bcea3447f4d13eaa3f
jonas.boecker <boecker@se-rwth.de>
2023-08-08 13:48:08 +0200

    add coco for public attributes in interfaces

---
+++ src/main/java/de/monticore/cdinterfaceandenum/cocos/ebnf/CDAttributeInInterfaceNotPublic.java
@@ -0,0 +1,27 @@
+/* (c) https://github.com/MontiCore/monticore */
+package de.monticore.cdinterfaceandenum.cocos.ebnf;
+
+import de.monticore.cdbasis._ast.ASTCDAttribute;
+import de.monticore.cdbasis._ast.ASTCDMember;
+import de.monticore.cdinterfaceandenum._ast.ASTCDInterface;
+import de.monticore.cdinterfaceandenum._cocos.CDInterfaceAndEnumASTCDInterfaceCoCo;
+import de.se_rwth.commons.logging.Log;
+
+public class CDAttributeInInterfaceNotPublic implements CDInterfaceAndEnumASTCDInterfaceCoCo {
+
+  public static final String ERROR_CODE = "0xCDCF7";
+
+  public static final String ERROR_MSG = "attributes in interface at %s must not be public";
+
+  @Override
+  public void check(ASTCDInterface node) {
+    for (ASTCDMember member : node.getCDMemberList()) {
+      if (member instanceof ASTCDAttribute) {
+        if (((ASTCDAttribute) member).getModifier().isPublic()) {
+          Log.error(String.format(ERROR_CODE + " " + ERROR_MSG, member.get_SourcePositionStart()));
+        }
+      }
+    }
+  }
+
+}
