d4b1cde34d8fd02a3c1ff7918cce8a8dd6b68164
tsvetinaangelova <tsvetina.angelova@rwth-aachen.de>
2023-09-28 09:05:29 +0200

    Update

--- src/cddiff/java/de/monticore/matcher/CombinedMatching.java
+++ src/cddiff/java/de/monticore/matcher/CombinedMatching.java
@@ -2,29 +2,47 @@ package de.monticore.matcher;
 
 import de.monticore.cdbasis._ast.ASTCDCompilationUnit;
 import de.monticore.cdbasis._ast.ASTCDType;
+import edu.mit.csail.sdg.alloy4.Pair;
 
-import java.util.ArrayList;
-import java.util.List;
+import java.util.*;
 
-public class CombinedMatching {
+public class CombinedMatching<T> {
 
-  /*List<MatchingStrategy<T>> matcherList = new ArrayList<>();
+  /*//A list with all matching strategies
+  //Tuk zavisi dali izpolzvame za assocs ili za types
+  List<MatchingStrategy<T>> matcherList = new ArrayList<>();
 
-  Map<T,T> getMatchMap(List<T> cd1ToMatch){
+  List<T> listWithAllEligableMatchingCandidates = new ArrayList<>();
+  //Syzdavame Map
+  //Kato input vzimame lista s neshta ot cd1 deto trqbva ada match-nem
+  Map<T,T> getMatchMap(List<T> cd1ToMatch) {
     Map map1 = new HashMap();
     List<T> cd2Matched = new ArrayList<>();
-    for(MatchingStrategy matcher : matcherList){
-      for(T srcElem : cd1ToMatch){
-        matcherList = matcher.getMatchedElements(srcElem);
-        matcherList.removeAll(cd2Matched);
+    for (MatchingStrategy<T> matcher : matcherList) {
+      for (T srcElem : cd1ToMatch) {
+        listWithAllEligableMatchingCandidates.addAll(matcher.getMatchedElements(srcElem));
+        List<Pair<Integer, T>> valuesForMatchingCandidates = new ArrayList<>();
+        for (T matchingCandidate : listWithAllEligableMatchingCandidates) {
+          //valuesForMatchingCandidates.add(new Pair<>(computeValueForMatching(matchingCandidate), matchingCandidate));
+        }
+        //Narejdame gi v nizhodqsh red spored prioriteta im
+        valuesForMatchingCandidates.sort(Comparator.comparing(p -> +p.a));
+        //Sled kato sa naredeni, A, B, C, proverqvame dali pyrviqt e v cd2Matched,
+        valuesForMatchingCandidates.removeIf(candidate -> cd2Matched.contains(candidate.b));
+        cd2Matched.add(valuesForMatchingCandidates.get(0).b);
 
-        if(!matcherList.isEmpty()){
-          map1.entrySet(srcElem, matcherList.get(0));
+
+        //matcherList.removeAll(cd2Matched);
+
+        if (!listWithAllEligableMatchingCandidates.isEmpty()) {
+          //map1.entrySet(srcElem, matcherList.get(0));
           cd1ToMatch.remove(srcElem);
-          cd2Matched.add(matcherList.get(0));
+          //
+          // cd2Matched.add(matcherList.get(0));
         }
       }
     }
+
 
   }*/
 }
