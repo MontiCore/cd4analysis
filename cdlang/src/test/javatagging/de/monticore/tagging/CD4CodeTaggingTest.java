6ae9bed9b1354e8261c3686cc13613b854420fd8
Alex Lüpges <luepges@se-rwth.de>
2023-11-23 11:52:19 +0100

    Changed regarding TagRepo

--- cdlang/src/test/javatagging/de/monticore/tagging/CD4CodeTaggingTest.java
+++ cdlang/src/test/javatagging/de/monticore/tagging/CD4CodeTaggingTest.java
@@ -11,6 +11,8 @@ import de.monticore.tagging.tags._ast.ASTTag;
 import de.monticore.tagging.tags._ast.ASTTagUnit;
 import de.se_rwth.commons.logging.Log;
 import de.se_rwth.commons.logging.LogStub;
+
+import java.io.File;
 import java.util.List;
 import java.util.Optional;
 import org.junit.jupiter.api.Assertions;
@@ -24,6 +26,7 @@ public class CD4CodeTaggingTest {
   public static void beforeAll() {
     LogStub.init();
     Log.enableFailQuick(false);
+    TagRepository.clearTags();
   }
 
   @BeforeEach
@@ -35,9 +38,8 @@ public class CD4CodeTaggingTest {
   public void test() throws Exception {
     CD4CodeTagDefinitionMill.init();
 
-    Optional<ASTTagUnit> tagDefOpt =
-        CD4CodeTagDefinitionMill.parser()
-            .parse("src/test/resources/de/monticore/tagging/Complete.tags");
+    File tagsFile = new File("src/test/resources/de/monticore/tagging/Complete.tags");
+    Optional<ASTTagUnit> tagDefOpt = TagRepository.loadTagModel(tagsFile);
     Assertions.assertTrue(tagDefOpt.isPresent());
     Assertions.assertEquals(0, Log.getErrorCount());
 
@@ -50,17 +52,17 @@ public class CD4CodeTaggingTest {
     Assertions.assertEquals(0, Log.getErrorCount());
 
     List<ASTTag> tags =
-        CD4CodeTagger.getInstance().getTags(astOpt.get().getCDDefinition(), tagDefOpt.get());
+        CD4CodeTagger.getInstance().getTags(astOpt.get().getCDDefinition());
     Assertions.assertEquals(1, tags.size());
 
     ASTCDClass classA =
         (ASTCDClass) astOpt.get().getEnclosingScope().resolveCDType("A").get().getAstNode();
 
-    tags = CDBasisTagger.getInstance().getTags(classA, tagDefOpt.get());
+    tags = CDBasisTagger.getInstance().getTags(classA);
     Assertions.assertEquals(1, tags.size());
 
     // Also check for CDTypeSymbols
-    tags = CDBasisTagger.getInstance().getTags(classA.getSymbol(), tagDefOpt.get());
+    tags = CDBasisTagger.getInstance().getTags(classA.getSymbol());
     Assertions.assertEquals(1, tags.size());
   }
 }
