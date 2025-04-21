1431f49a9014e3e54afef4d7e2059b045d9a00c3
Alexander Hellwig <hellwig@se-rwth.de>
2025-04-21 16:15:40 +0200

    Move faster getCDMemberList impl up to ASTCDType

--- cdlang/src/main/java/de/monticore/cdbasis/_ast/ASTCDClass.java
+++ cdlang/src/main/java/de/monticore/cdbasis/_ast/ASTCDClass.java
@@ -1,14 +1,12 @@
 /* (c) https://github.com/MontiCore/monticore */
 package de.monticore.cdbasis._ast;
 
-import de.monticore.cd._visitor.CDMemberVisitor;
 import de.monticore.cd.prettyprint.PrettyPrintUtil;
 import de.monticore.types.mcbasictypes._ast.ASTMCObjectType;
 
-import java.util.*;
+import java.util.Collections;
+import java.util.List;
 import java.util.stream.Collectors;
-
-import static de.monticore.cd._visitor.CDMemberVisitor.Options.*;
 
 public class ASTCDClass extends ASTCDClassTOP {
 
@@ -59,72 +57,5 @@ public class ASTCDClass extends ASTCDClassTOP {
     return getCDInterfaceUsage().getInterfaceList().stream()
         .map(ASTMCObjectType::printType)
         .collect(Collectors.joining(","));
-  }
-
-  /**
-   * Since the CD Generator calls this method a lot, a handwritten, much faster version is implemented, which does not
-   * use the generated visitors.
-   */
-  @Override
-  public <T extends ASTCDMember> List<T> getCDMemberList(CDMemberVisitor.Options option, CDMemberVisitor.Options... options) {
-    List<CDMemberVisitor.Options> allOptions = new ArrayList<>();
-    allOptions.add(option);
-    allOptions.addAll(Arrays.asList(options));
-
-    Set<T> res = new LinkedHashSet<>();
-
-    if(allOptions.contains(ALL)){
-      res.addAll((Collection<? extends T>) getCDMemberList());
-    }
-
-    if(allOptions.contains(FIELDS)){
-      for (ASTCDMember m : getCDMemberList()) {
-        if (m.isField()) {
-          res.add((T) m);
-        }
-      }
-    }
-
-    if(allOptions.contains(ATTRIBUTES)){
-      for (ASTCDMember m : getCDMemberList()) {
-        if (m.isAttribute()) {
-          res.add((T) m);
-        }
-      }
-    }
-
-    if(allOptions.contains(ROLES)){
-      for (ASTCDMember m : getCDMemberList()) {
-        if (m.isRole()) {
-          res.add((T) m);
-        }
-      }
-    }
-
-    if(allOptions.contains(METHOD_SIGNATURES)){
-      for (ASTCDMember m : getCDMemberList()) {
-        if (m.isMethodSignature()) {
-          res.add((T) m);
-        }
-      }
-    }
-
-    if(allOptions.contains(CONSTRUCTORS)){
-      for (ASTCDMember m : getCDMemberList()) {
-        if (m.isConstructor()) {
-          res.add((T) m);
-        }
-      }
-    }
-
-    if(allOptions.contains(METHODS)){
-      for (ASTCDMember m : getCDMemberList()) {
-        if (m.isMethod()) {
-          res.add((T) m);
-        }
-      }
-    }
-
-    return new ArrayList<>(res);
   }
 }
