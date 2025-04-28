e32cc9afc3a6a4ea41ab1db03e5b52fb020a84b7
Felix Jordan <felix.jordan@yukawa.de>
2025-04-28 15:14:02 +0200

    refactor(cdconc): rename getAttributeIncStrategy to createAttributeIncStrategy

--- cddiff/src/main/java/de/monticore/cdconcretization/type/TypeCompletionContext.java
+++ cddiff/src/main/java/de/monticore/cdconcretization/type/TypeCompletionContext.java
@@ -5,17 +5,26 @@ import de.monticore.cdbasis._ast.ASTCDType;
 import de.monticore.cdconcretization.cd.CDCompletionContext;
 import de.monticore.cdmatcher.MatchingStrategy;
 
+/**
+ * A {@link CDCompletionContext} with additional information when completing a single type.
+ */
 public interface TypeCompletionContext extends CDCompletionContext {
 
+  /**
+   * @return the concrete type that is currently being completed.
+   */
   ASTCDType getConcreteType();
 
+  /**
+   * @return the reference type that is used to complete the concrete type.
+   */
   ASTCDType getReferenceType();
 
   /**
    * Returns the matching strategy for the attribute incarnations. The strategy returned here is
    * only valid in the current type context!
    *
-   * @return
+   * @return the matching strategy for the attribute incarnations.
    */
   MatchingStrategy<ASTCDAttribute> getAttributeIncStrategy();
 }
