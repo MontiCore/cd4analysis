0c5860efbe1faea827f989f6ffd7a7d4e7e412bd
Felix Jordan <felix.jordan@yukawa.de>
2025-04-28 15:48:16 +0200

    refactor(cdconc): rename forEach target element to hook element

--- cddiff/src/main/java/de/monticore/cdconcretization/type/attribute/ForEachTypeAttributeCompleter.java
+++ cddiff/src/main/java/de/monticore/cdconcretization/type/attribute/ForEachTypeAttributeCompleter.java
@@ -19,10 +19,11 @@ import java.util.stream.Collectors;
 
 /**
  * Processes reference attributes that are annotated with the stereotype 'forEach'. The value of the
- * stereotype is expected to be a reference to another model element. The reference element (target)
- * is resolved and for each incarnation of the target element one attribute incarnation is created
- * parameterized by the target incarnation.<br>
- * Currently supported target references are:
+ * stereotype is expected to be a reference to another model element (hook element). For each
+ * incarnation of this hook element, one attribute incarnation is created parameterized by the hook
+ * element incarnation.<br>
+ * <br>
+ * Currently supported hook elements are:
  *
  * <ul>
  *   <li>attributes (e.g., 'Foo.attr')
@@ -45,8 +46,8 @@ public class ForEachTypeAttributeCompleter extends AbstractTypeAttributeComplete
     if (stereotypeValue.isPresent()) {
       CDRefSymbolHandlerDelegator symbolHandler = new CDRefSymbolHandlerDelegator();
       symbolHandler.setAttributeHandler(
-          rTargetAttribute -> completeAttributeUsingAttribute(referenceAttribute, rTargetAttribute, context));
-      // TODO Add support for other references
+          hookAttribute -> completeAttributeUsingAttribute(referenceAttribute, hookAttribute, context));
+      // TODO Add support for other hook elements
       symbolHandler.resolveSymbol(
           context.getReferenceCD().getEnclosingScope(),
           stereotypeValue.get(),
@@ -58,36 +59,36 @@ public class ForEachTypeAttributeCompleter extends AbstractTypeAttributeComplete
   }
 
   /**
-   * Creates a new attribute for each incarnation of the target attribute. If the declaring type of
-   * the target attribute has multiple incarnations, the new attributes will additionally be
+   * Creates a new attribute for each incarnation of the hook attribute. If the declaring type of
+   * the hook attribute has multiple incarnations, the new attributes will additionally be
    * parameterized by the declaring type incarnation.<br>
    * <br>
    * Rules for the new attribute incarnations:
    *
    * <ul>
-   *   <li>If the reference and target attribute names match, the attribute incarnation has the name
-   *       of the target incarnation
+   *   <li>If the reference and hook attribute names match, the attribute incarnation has the name
+   *       of the hook incarnation
    *   <li>Otherwise, each attribute incarnation has the name of the reference attribute with a
-   *       suffix of the target attribute name
-   *   <li>If the reference and target attribute types match, the attribute incarnation has the type
-   *       of the target attribute
+   *       suffix of the hook attribute name
+   *   <li>If the reference and hook attribute types match, the attribute incarnation has the type
+   *       of the hook attribute
    *   <li>Otherwise, each attribute incarnation has the type of the reference attribute
    * </ul>
    *
    * @param referenceAttribute the original reference attribute
-   * @param rTargetAttribute the target of the expression used in the forEach stereotype. The
+   * @param hookAttribute the attribute referenced in the forEach stereotype. The
    *     attribute by which this construction is parameterized.
    * @param context the completion context
    */
   private void completeAttributeUsingAttribute(
       ASTCDAttribute referenceAttribute,
-      ASTCDAttribute rTargetAttribute, // TODO maybe name paramAttribute instead of 'target' ?
+      ASTCDAttribute hookAttribute,
       TypeCompletionContext context)
       throws CompletionException {
 
     // group attribute incarnations by their declaring type
     Map<CDTypeSymbol, List<ASTCDAttribute>> attributesByDeclaringType =
-            context.getAttributeIncarnations(rTargetAttribute).stream()
+            context.getAttributeIncarnations(hookAttribute).stream()
             .collect(
                 Collectors.groupingBy(
                     attr -> (CDTypeSymbol) attr.getEnclosingScope().getSpanningSymbol()));
@@ -96,43 +97,43 @@ public class ForEachTypeAttributeCompleter extends AbstractTypeAttributeComplete
     // if there is no incarnation of the declaring type, we do not need to create any new attributes
     for (Map.Entry<CDTypeSymbol, List<ASTCDAttribute>> entry :
         attributesByDeclaringType.entrySet()) {
-      CDTypeSymbol cAttributeDeclaringType = entry.getKey();
-      List<ASTCDAttribute> attributeIncarnations = entry.getValue();
+      CDTypeSymbol hookIncarnationDeclaringType = entry.getKey();
+      List<ASTCDAttribute> hookIncarnations = entry.getValue();
 
       // if we have more than one declaring type incarnation, we need to add a suffix to the new
       // attributes
-      String declaringTypeNameWithoutCDQualifier = SymbolUtil.getFullNameWithoutCD(cAttributeDeclaringType);
+      String declaringTypeNameWithoutCDQualifier = SymbolUtil.getFullNameWithoutCD(hookIncarnationDeclaringType);
       String declaringTypeSuffix =
               attributesByDeclaringType.size() > 1
               ? "_" + NameUtil.escapeQualifiedNameAsIdentifier(declaringTypeNameWithoutCDQualifier)
               : "";
 
-      for (ASTCDAttribute cAttribute : attributeIncarnations) {
-        // now we have a specific incarnation of the reference attribute in the concrete CD.
+      for (ASTCDAttribute hookIncarnation : hookIncarnations) {
+        // now we have a specific incarnation of the hook attribute in the concrete CD.
         // we can now construct a new attribute based on this incarnation for the concrete type
 
-        // if we have more than one attribute type incarnation, we need to add a suffix to the
+        // if we have more than one declaring type incarnation, we need to add a suffix to the
         // new attributes
-        String attributeSuffix = attributeIncarnations.size() > 1 ? "_" + cAttribute.getName() : "";
+        String attributeSuffix = hookIncarnations.size() > 1 ? "_" + hookIncarnation.getName() : "";
 
         ASTCDAttribute newAttribute = referenceAttribute.deepClone();
 
         // 1. decide name of the new attribute
-        if (referenceAttribute.getName().equals(rTargetAttribute.getName())) {
-          // Convention: If the REFERENCED attribute name matches the reference attribute name
-          // -> Use the REFERENCED name without a suffix (but still a type suffix)
-          newAttribute.setName(cAttribute.getName() + declaringTypeSuffix);
+        if (referenceAttribute.getName().equals(hookAttribute.getName())) {
+          // Convention: If the hook attribute name matches the reference attribute name
+          // -> Use the hook incarnation name without a suffix (but still a type suffix)
+          newAttribute.setName(hookIncarnation.getName() + declaringTypeSuffix);
         } else {
-          // Default: add the REFERENCED attribute incarnation name as suffix
+          // Default: add the hook incarnation name as suffix
           newAttribute.setName(
                   referenceAttribute.getName() + declaringTypeSuffix + attributeSuffix);
         }
 
         // 2. decide type of the new attribute
-        if (referenceAttribute.getMCType().deepEquals(rTargetAttribute.getMCType())) {
-          // Convention: If the REFERENCED attribute type matches the reference attribute type
-          // -> Use the REFERENCED type for every incarnation
-          newAttribute.setMCType(cAttribute.getMCType());
+        if (referenceAttribute.getMCType().deepEquals(hookAttribute.getMCType())) {
+          // Convention: If the hook attribute type matches the reference attribute type
+          // -> Use the hook incarnation type for every incarnation
+          newAttribute.setMCType(hookIncarnation.getMCType());
         } else {
           // Default: keep the type of the reference attribute resp.
           /*
@@ -156,16 +157,16 @@ public class ForEachTypeAttributeCompleter extends AbstractTypeAttributeComplete
         if (ADD_BINDING_STEREOTYPE_TO_ATTRIBUTES) {
           StereotypeUtil.addIncarnationBindingStereotype(
                   newAttribute.getModifier(),
-                  rTargetAttribute.getSymbol().getFullName(),
-                  SymbolUtil.getFullNameWithoutCD(cAttribute.getSymbol()));
+                  hookAttribute.getSymbol().getFullName(),
+                  SymbolUtil.getFullNameWithoutCD(hookIncarnation.getSymbol()));
         }
 
-        String newAttrQualifier = cAttributeDeclaringType.getFullName();
+        String newAttrQualifier = hookIncarnationDeclaringType.getFullName();
         String newAttrFullName = Names.getQualifiedName(newAttrQualifier, newAttribute.getName());
         context.getScopedIncarnationBindings().addFieldBinding(
             newAttrFullName,
-            rTargetAttribute.getSymbol(),
-            Set.of(cAttribute.getSymbol()));
+            hookAttribute.getSymbol(),
+            Set.of(hookIncarnation.getSymbol()));
 
         // 4. pass the new attribute to the next completer
         super.completeTypeForAttribute(context.getConcreteType(), newAttribute, context);
