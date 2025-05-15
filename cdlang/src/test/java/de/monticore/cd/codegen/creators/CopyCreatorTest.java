de61b251e81970f5ccb0396a453e6bb5cfef1fa0
Johannes Leurs <johannes.leurs@rwth-aachen.de>
2025-05-15 14:55:57 +0200

    Fix CopyCreator for processing of more than one model

---
+++ cdlang/src/test/java/de/monticore/cd/codegen/creators/CopyCreatorTest.java
@@ -0,0 +1,39 @@
+/* (c) https://github.com/MontiCore/monticore */
+package de.monticore.cd.codegen.creators;
+
+import de.monticore.cd.codegen.decorators.data.DecoratorData;
+import de.monticore.cd4code.CD4CodeTestBasis;
+import de.monticore.cdbasis._ast.ASTCDCompilationUnit;
+import org.junit.jupiter.api.Test;
+
+import java.util.Optional;
+
+import static org.junit.jupiter.api.Assertions.assertAll;
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+/** Tests {@link CopyCreator} */
+class CopyCreatorTest extends CD4CodeTestBasis {
+
+  @Test
+  void testSequentialApplication() {
+    // Given
+    ASTCDCompilationUnit auctionCUnit = parse("cd/codegen/GenAuction.cd");
+    ASTCDCompilationUnit automatonCUnit = parse("cd/codegen/Automaton.cd");
+
+    DecoratorData decoratorData = new DecoratorData();
+    CopyCreator creatingDecorator = new CopyCreator();
+    creatingDecorator.init(decoratorData, Optional.empty());
+
+    // When
+    creatingDecorator.visit(auctionCUnit);
+    ASTCDCompilationUnit decoratedAuction = decoratorData.getAsDecorated(auctionCUnit);
+    creatingDecorator.visit(automatonCUnit);
+    ASTCDCompilationUnit decoratedAutomaton = decoratorData.getAsDecorated(automatonCUnit);
+
+    // Then
+    assertAll(
+      () -> assertEquals("GenAuction", decoratedAuction.getCDDefinition().getName()),
+      () -> assertEquals("Automaton", decoratedAutomaton.getCDDefinition().getName())
+    );
+  }
+}
