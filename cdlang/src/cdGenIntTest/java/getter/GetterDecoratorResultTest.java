7ca0789744668e570fbc5f17c88bd65467ac6ed1
Alex Lüpges <luepges@se-rwth.de>
2025-05-27 07:57:30 +0200

    Cleanup GetterDecoratorResultTest

--- cdlang/src/cdGenIntTest/java/getter/GetterDecoratorResultTest.java
+++ cdlang/src/cdGenIntTest/java/getter/GetterDecoratorResultTest.java
@@ -1,12 +1,10 @@
 /* (c) https://github.com/MontiCore/monticore */
 package getter;
 
-import TestGetter.Other;
+
 import java.lang.reflect.Method;
 import java.lang.reflect.Modifier;
-import java.math.BigInteger;
-import java.util.List;
-import java.util.Set;
+
 import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.Test;
 
@@ -24,13 +22,11 @@ public class GetterDecoratorResultTest {
     obj.__setMyInt(42);
     Assertions.assertEquals(42, obj.getMyInt());
 
-    // Check if the boolean is prefixed with is & has the package default visibility
+    // Check if the boolean is prefixed with is & has the "public" default visibility (via default)
     Method isMyBool = TestGetter.TestGetterC.class.getDeclaredMethod("isMyBool");
-    var modifier = BigInteger.valueOf(isMyBool.getModifiers());
-    Assertions.assertFalse(modifier.testBit(Modifier.PUBLIC));
-    Assertions.assertFalse(modifier.testBit(Modifier.PRIVATE));
-    Assertions.assertFalse(modifier.testBit(Modifier.PROTECTED));
-    Assertions.assertEquals(0, modifier.intValue());
+    Assertions.assertTrue(Modifier.isPublic(isMyBool.getModifiers()));
+    Assertions.assertFalse(Modifier.isPrivate(isMyBool.getModifiers()));
+    Assertions.assertFalse(Modifier.isProtected(isMyBool.getModifiers()));
 
     // Test NoGetter / public
     Assertions.assertEquals(0, obj.pubX);
@@ -38,16 +34,16 @@ public class GetterDecoratorResultTest {
     // Ensure no getPubX() method exists
     //noinspection JavaReflectionMemberAccess
     Assertions.assertThrows(
-        java.lang.NoSuchMethodException.class,
-        () -> TestGetter.TestGetterC.class.getDeclaredMethod("getPubX"));
+      java.lang.NoSuchMethodException.class,
+      () -> TestGetter.TestGetterC.class.getDeclaredMethod("getPubX"));
 
     // Test that the CardinalityDefaultDecorator run correctly
-    Assertions.assertNotNull(obj.__getRoleB());
-    Assertions.assertNotNull(obj.__getOrderedRole());
+    Assertions.assertNotNull(obj.getRoleB());
+    Assertions.assertNotNull(obj.getOrderedRole());
 
     // Test the initial size of lists/sets
-    Assertions.assertEquals(0, obj.__getRoleB().size());
-    Assertions.assertEquals(0, obj.__getOrderedRole().size());
+    Assertions.assertEquals(0, obj.getRoleB().size());
+    Assertions.assertEquals(0, obj.getOrderedRole().size());
   }
 
   // Add a setter for tests
@@ -55,14 +51,6 @@ public class GetterDecoratorResultTest {
 
     protected void __setMyInt(int i) {
       this.myInt = i;
-    }
-
-    protected Set<Other> __getRoleB() {
-      return this.roleB;
-    }
-
-    protected List<Other> __getOrderedRole() {
-      return this.orderedRole;
     }
   }
 }
