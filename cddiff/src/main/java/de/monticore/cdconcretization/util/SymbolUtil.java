f0e1f655b4337649f28153a48799e26c6fa519f1
Felix Jordan <felix.jordan1@rwth-aachen.de>
2025-08-01 15:10:49 +0200

    refactor(cdconc): accept all kinds of symbols in getDeclaringTypeSymbol

--- cddiff/src/main/java/de/monticore/cdconcretization/util/SymbolUtil.java
+++ cddiff/src/main/java/de/monticore/cdconcretization/util/SymbolUtil.java
@@ -17,10 +17,10 @@ public class SymbolUtil {
   
   private SymbolUtil() {}
   
-  public static TypeSymbol getDeclaringTypeSymbol(FieldSymbol fieldSymbol) {
-    IScope enclosingScope = fieldSymbol.getEnclosingScope();
+  public static TypeSymbol getDeclaringTypeSymbol(ISymbol iSymbol) {
+    IScope enclosingScope = iSymbol.getEnclosingScope();
     if (!enclosingScope.isPresentSpanningSymbol()) {
-      throw new IllegalStateException("FieldSymbol without enclosing scope: " + fieldSymbol
+      throw new IllegalStateException("Symbol without enclosing scope: " + iSymbol
           .getFullName());
     }
     IScopeSpanningSymbol symbol = enclosingScope.getSpanningSymbol();
@@ -28,27 +28,11 @@ public class SymbolUtil {
       return (TypeSymbol) symbol;
     }
     else {
-      throw new IllegalStateException("FieldSymbol not enclosed in a type symbol: " + fieldSymbol
+      throw new IllegalStateException("Symbol not enclosed in a type symbol: " + iSymbol
           .getFullName());
     }
   }
-  
-  public static TypeSymbol getDeclaringTypeSymbol(MethodSymbol methodSymbol) {
-    IScope enclosingScope = methodSymbol.getEnclosingScope();
-    if (!enclosingScope.isPresentSpanningSymbol()) {
-      throw new IllegalStateException("MethodSymbol without enclosing scope: " + methodSymbol
-          .getFullName());
-    }
-    IScopeSpanningSymbol symbol = enclosingScope.getSpanningSymbol();
-    if (symbol instanceof TypeSymbol) {
-      return (TypeSymbol) symbol;
-    }
-    else {
-      throw new IllegalStateException("MethodSymbol not enclosed in a type symbol: " + methodSymbol
-          .getFullName());
-    }
-  }
-  
+
   public static String getFullNameWithoutCD(ISymbol symbol) {
     return NameUtil.removeFirstQualifierPart(symbol.getFullName());
   }
