d9a1d793bcd666f1b0027b4b7d02b68d3750bb7f
Tom Bursch <bursch@se-rwth.de>
2025-08-13 12:58:53 +0200

    Remove cd4code references in cd4a

--- cdlang/src/main/java/de/monticore/cd4analysis/_symboltable/CD4AnalysisSymbolTableCompleter.java
+++ cdlang/src/main/java/de/monticore/cd4analysis/_symboltable/CD4AnalysisSymbolTableCompleter.java
@@ -2,9 +2,9 @@
 package de.monticore.cd4analysis._symboltable;
 
 import de.monticore.cd.facade.MCQualifiedNameFacade;
+import de.monticore.cd4analysis.CD4AnalysisMill;
 import de.monticore.cd4analysis._visitor.CD4AnalysisTraverser;
-import de.monticore.cd4code.CD4CodeMill;
-import de.monticore.cd4code.typescalculator.FullSynthesizeFromCD4Code;
+import de.monticore.cd4analysis.typescalculator.FullSynthesizeFromCD4Analysis;
 import de.monticore.cdassociation._symboltable.CDAssociationSymbolTableCompleter;
 import de.monticore.cdbasis._ast.ASTCDCompilationUnit;
 import de.monticore.cdbasis._symboltable.CDBasisSymbolTableCompleter;
@@ -14,32 +14,32 @@ import de.monticore.types.mcbasictypes._ast.ASTMCQualifiedName;
 import java.util.List;
 
 public class CD4AnalysisSymbolTableCompleter {
-  
+
   protected CD4AnalysisTraverser traverser;
-  
+
   public CD4AnalysisSymbolTableCompleter(ASTCDCompilationUnit ast) {
     this(ast.getMCImportStatementList(), ast.isPresentMCPackageDeclaration() ? ast
         .getMCPackageDeclaration().getMCQualifiedName() : MCQualifiedNameFacade.createQualifiedName(
             ""));
   }
-  
+
   public CD4AnalysisSymbolTableCompleter(List<ASTMCImportStatement> imports,
       ASTMCQualifiedName packageDeclaration) {
-    this.traverser = CD4CodeMill.inheritanceTraverser();
-    
+    this.traverser = CD4AnalysisMill.inheritanceTraverser();
+
     final CDBasisSymbolTableCompleter cDBasisVisitor = new CDBasisSymbolTableCompleter(
-        new FullSynthesizeFromCD4Code());
+        new FullSynthesizeFromCD4Analysis());
     traverser.add4CDBasis(cDBasisVisitor);
     traverser.add4OOSymbols(cDBasisVisitor);
     final CDAssociationSymbolTableCompleter cDAssociationVisitor =
-        new CDAssociationSymbolTableCompleter(new FullSynthesizeFromCD4Code());
+        new CDAssociationSymbolTableCompleter(new FullSynthesizeFromCD4Analysis());
     traverser.add4CDAssociation(cDAssociationVisitor);
     traverser.setCDAssociationHandler(cDAssociationVisitor);
     final CDInterfaceAndEnumSymbolTableCompleter cdInterfaceAndEnumVisitor =
-        new CDInterfaceAndEnumSymbolTableCompleter(new FullSynthesizeFromCD4Code());
+        new CDInterfaceAndEnumSymbolTableCompleter(new FullSynthesizeFromCD4Analysis());
     traverser.add4CDInterfaceAndEnum(cdInterfaceAndEnumVisitor);
   }
-  
+
   public CD4AnalysisTraverser getTraverser() { return traverser; }
-  
+
 }
