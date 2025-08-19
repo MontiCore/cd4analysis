82555a373956a6cb5c604552c765f8b1979500f6
Max Philip Stachon <max.stachon@rwth-aachen.de>
2025-08-19 18:21:00 +0200

    fix SyntaxDiffTest.testMaCoCo2

--- cddiff/src/test/java/de/monticore/cddiff/syndiff/Syn2SemDiffTest.java
+++ cddiff/src/test/java/de/monticore/cddiff/syndiff/Syn2SemDiffTest.java
@@ -28,10 +28,10 @@ import static org.junit.jupiter.api.Assertions.assertFalse;
 import static org.junit.jupiter.api.Assertions.assertTrue;
 
 public class Syn2SemDiffTest extends SynDiffTestBasis {
-
+  
   private static final String cddiffDir = "src/test/resources/de/monticore/cddiff/";
   private static final String validationDir = "src/test/resources/validation/";
-
+  
   public static Stream<Arguments> emptyWitnesses() {
     return Stream.of(Arguments.of(cddiffDir, "syndiff/AssocDiff/AssocDeletedMerging/CD51.cd",
         "syndiff/AssocDiff/AssocDeletedMerging/CD52.cd", false, false), Arguments.of(cddiffDir,
@@ -91,114 +91,118 @@ public class Syn2SemDiffTest extends SynDiffTestBasis {
         Arguments.of(validationDir, "cd4analysis/MyExampleV1.cd", "cd4analysis/MyExampleV2.cd",
             true, false));
   }
-
+  
   @ParameterizedTest
   @MethodSource("emptyWitnesses")
   public void testEmptyWitnesses(String baseDir, String srcPath, String tgtPath, boolean staDiff,
       boolean reduction) {
     dir = baseDir;
     parseModels(srcPath, tgtPath);
-
+    
     if (reduction) {
       ReductionTrafo trafo = new ReductionTrafo();
       trafo.transform(src, tgt);
     }
-
+    
     Syn2SemDiff syn2semdiff = new Syn2SemDiff(src, tgt);
     List<ASTODArtifact> witnesses = syn2semdiff.generateODs(staDiff);
-
+    
     assertTrue(witnesses.isEmpty());
   }
-
+  
   public static Stream<Arguments> witnesses() {
-    return Stream.of(
-        Arguments.of(cddiffDir, "syndiff/AssocDiff/AssocDeletedMerging/CD51.cd", "syndiff/AssocDiff/AssocDeletedMerging/CD52.cd", SIMPLE_CLOSED_WORLD, true),
-        Arguments.of(validationDir, "cddiff/LibraryV5.cd", "cddiff/LibraryV4.cd", STA_CLOSED_WORLD, false),
-        Arguments.of(cddiffDir, "Employees/Employees1.cd", "Employees/Employees2.cd", SIMPLE_CLOSED_WORLD, true),
-        Arguments.of(cddiffDir, "syndiff/SyntaxDiff/AddedDeletedAssocs/CD31.cd",
-            "syndiff/SyntaxDiff/AddedDeletedAssocs/CD32.cd", SIMPLE_CLOSED_WORLD, false), Arguments
-                .of(cddiffDir, "syndiff/SyntaxDiff/MoveAttributes/CD12.cd",
-                    "syndiff/SyntaxDiff/MoveAttributes/CD11.cd", SIMPLE_CLOSED_WORLD, true),
-        Arguments.of(cddiffDir, "syndiff/TypeDiff/DeletedAttribute/CD11.cd",
-            "syndiff/TypeDiff/DeletedAttribute/CD12.cd", SIMPLE_CLOSED_WORLD, false), Arguments.of(
+    return Stream.of(Arguments.of(cddiffDir, "syndiff/AssocDiff/AssocDeletedMerging/CD51.cd",
+        "syndiff/AssocDiff/AssocDeletedMerging/CD52.cd", SIMPLE_CLOSED_WORLD, true), Arguments.of(
+            validationDir, "cddiff/LibraryV5.cd", "cddiff/LibraryV4.cd", STA_CLOSED_WORLD, false),
+        Arguments.of(cddiffDir, "Employees/Employees1.cd", "Employees/Employees2.cd",
+            SIMPLE_CLOSED_WORLD, true), Arguments.of(cddiffDir,
+                "syndiff/SyntaxDiff/AddedDeletedAssocs/CD31.cd",
+                "syndiff/SyntaxDiff/AddedDeletedAssocs/CD32.cd", SIMPLE_CLOSED_WORLD, false),
+        Arguments.of(cddiffDir, "syndiff/SyntaxDiff/MoveAttributes/CD12.cd",
+            "syndiff/SyntaxDiff/MoveAttributes/CD11.cd", SIMPLE_CLOSED_WORLD, true), Arguments.of(
                 cddiffDir, "syndiff/TypeDiff/DeletedAttribute/CD11.cd",
-                "syndiff/TypeDiff/DeletedAttribute/CD12.cd", SIMPLE_CLOSED_WORLD, true), Arguments
-                    .of(cddiffDir, "syndiff/TypeDiff/RemovedAddedAttributeDiff/CD31.cd",
-                        "syndiff/TypeDiff/RemovedAddedAttributeDiff/CD32.cd", SIMPLE_CLOSED_WORLD,
-                        false), Arguments.of(cddiffDir,
-                            "syndiff/TypeDiff/RemovedAddedAttributeDiff/CD31.cd",
-                            "syndiff/TypeDiff/RemovedAddedAttributeDiff/CD32.cd",
-                            SIMPLE_CLOSED_WORLD, true), Arguments.of(cddiffDir,
-                                "syndiff/TypeDiff/RemovedAttributeNoDiff/CD21.cd",
-                                "syndiff/TypeDiff/RemovedAttributeNoDiff/CD22.cd",
+                "syndiff/TypeDiff/DeletedAttribute/CD12.cd", SIMPLE_CLOSED_WORLD, false), Arguments
+                    .of(cddiffDir, "syndiff/TypeDiff/DeletedAttribute/CD11.cd",
+                        "syndiff/TypeDiff/DeletedAttribute/CD12.cd", SIMPLE_CLOSED_WORLD, true),
+        Arguments.of(cddiffDir, "syndiff/TypeDiff/RemovedAddedAttributeDiff/CD31.cd",
+            "syndiff/TypeDiff/RemovedAddedAttributeDiff/CD32.cd", SIMPLE_CLOSED_WORLD, false),
+        Arguments.of(cddiffDir, "syndiff/TypeDiff/RemovedAddedAttributeDiff/CD31.cd",
+            "syndiff/TypeDiff/RemovedAddedAttributeDiff/CD32.cd", SIMPLE_CLOSED_WORLD, true),
+        Arguments.of(cddiffDir, "syndiff/TypeDiff/RemovedAttributeNoDiff/CD21.cd",
+            "syndiff/TypeDiff/RemovedAttributeNoDiff/CD22.cd", SIMPLE_CLOSED_WORLD, true), Arguments
+                .of(cddiffDir, "syndiff/SyntaxDiff/MaCoCo_v1.cd", "syndiff/SyntaxDiff/MaCoCo_v2.cd",
+                    SIMPLE_CLOSED_WORLD, false), Arguments.of(cddiffDir,
+                        "syndiff/SyntaxDiff/SS1.cd", "syndiff/SyntaxDiff/SS2.cd",
+                        SIMPLE_CLOSED_WORLD, false), Arguments.of(cddiffDir,
+                            "DigitalTwins/DigitalTwin2.cd", "DigitalTwins/DigitalTwin1.cd",
+                            SIMPLE_CLOSED_WORLD, false), Arguments.of(cddiffDir,
+                                "DigitalTwins/DigitalTwin1.cd", "DigitalTwins/DigitalTwin2.cd",
                                 SIMPLE_CLOSED_WORLD, true), Arguments.of(cddiffDir,
-                                    "syndiff/SyntaxDiff/MaCoCo_v1.cd",
-                                    "syndiff/SyntaxDiff/MaCoCo_v2.cd", SIMPLE_CLOSED_WORLD, false),
-        Arguments.of(cddiffDir, "syndiff/SyntaxDiff/SS1.cd", "syndiff/SyntaxDiff/SS2.cd",
-            SIMPLE_CLOSED_WORLD, false), Arguments.of(cddiffDir, "DigitalTwins/DigitalTwin2.cd",
-                "DigitalTwins/DigitalTwin1.cd", SIMPLE_CLOSED_WORLD, false), Arguments.of(cddiffDir,
-                    "DigitalTwins/DigitalTwin1.cd", "DigitalTwins/DigitalTwin2.cd",
-                    SIMPLE_CLOSED_WORLD, true), Arguments.of(cddiffDir,
-                        "DigitalTwins/DigitalTwin2.cd", "DigitalTwins/DigitalTwin3.cd",
-                        SIMPLE_CLOSED_WORLD, true), Arguments.of(cddiffDir,
-                            "DigitalTwins/DigitalTwin3.cd", "DigitalTwins/DigitalTwin2.cd",
-                            SIMPLE_CLOSED_WORLD, true), Arguments.of(cddiffDir,
-                                "DigitalTwins/DigitalTwin2.cd", "DigitalTwins/DigitalTwin1.cd",
-                                SIMPLE_OPEN_WORLD, false), Arguments.of(cddiffDir,
-                                    "DigitalTwins/DigitalTwin1.cd", "DigitalTwins/DigitalTwin2.cd",
-                                    SIMPLE_OPEN_WORLD, true), Arguments.of(cddiffDir,
-                                        "DigitalTwins/DigitalTwin2.cd",
-                                        "DigitalTwins/DigitalTwin3.cd", SIMPLE_OPEN_WORLD, true),
-        Arguments.of(cddiffDir, "DigitalTwins/DigitalTwin3.cd", "DigitalTwins/DigitalTwin2.cd",
-            SIMPLE_OPEN_WORLD, true), Arguments.of(cddiffDir, "Employees/Employees1.cd",
-                "Employees/Employees2.cd", SIMPLE_CLOSED_WORLD, false), Arguments.of(cddiffDir,
-                    "Employees/Employees2.cd", "Employees/Employees1.cd", SIMPLE_CLOSED_WORLD,
-                    false), Arguments.of(cddiffDir, "Employees/Employees7.cd",
-                        "Employees/Employees8.cd", STA_CLOSED_WORLD, true), Arguments.of(cddiffDir,
-                            "Employees/Employees7.cd", "Employees/Employees8.cd", STA_CLOSED_WORLD,
-                            false), Arguments.of(cddiffDir, "Employees/Employees8.cd",
-                                "Employees/Employees7.cd", STA_CLOSED_WORLD, false), Arguments.of(
-                                    validationDir, "cddiff/LibraryV2.cd", "cddiff/LibraryV3.cd",
-                                    STA_CLOSED_WORLD, true), Arguments.of(validationDir,
-                                        "cddiff/LibraryV4.cd", "cddiff/LibraryV5.cd",
-                                        STA_CLOSED_WORLD, true), Arguments.of(validationDir,
-                                            "cddiff/LibraryV3.cd", "cddiff/LibraryV2.cd",
+                                    "DigitalTwins/DigitalTwin2.cd", "DigitalTwins/DigitalTwin3.cd",
+                                    SIMPLE_CLOSED_WORLD, true), Arguments.of(cddiffDir,
+                                        "DigitalTwins/DigitalTwin3.cd",
+                                        "DigitalTwins/DigitalTwin2.cd", SIMPLE_CLOSED_WORLD, true),
+        Arguments.of(cddiffDir, "DigitalTwins/DigitalTwin2.cd", "DigitalTwins/DigitalTwin1.cd",
+            SIMPLE_OPEN_WORLD, false), Arguments.of(cddiffDir, "DigitalTwins/DigitalTwin1.cd",
+                "DigitalTwins/DigitalTwin2.cd", SIMPLE_OPEN_WORLD, true), Arguments.of(cddiffDir,
+                    "DigitalTwins/DigitalTwin2.cd", "DigitalTwins/DigitalTwin3.cd",
+                    SIMPLE_OPEN_WORLD, true), Arguments.of(cddiffDir,
+                        "DigitalTwins/DigitalTwin3.cd", "DigitalTwins/DigitalTwin2.cd",
+                        SIMPLE_OPEN_WORLD, true), Arguments.of(cddiffDir, "Employees/Employees1.cd",
+                            "Employees/Employees2.cd", SIMPLE_CLOSED_WORLD, false), Arguments.of(
+                                cddiffDir, "Employees/Employees2.cd", "Employees/Employees1.cd",
+                                SIMPLE_CLOSED_WORLD, false), Arguments.of(cddiffDir,
+                                    "Employees/Employees7.cd", "Employees/Employees8.cd",
+                                    STA_CLOSED_WORLD, true), Arguments.of(cddiffDir,
+                                        "Employees/Employees7.cd", "Employees/Employees8.cd",
+                                        STA_CLOSED_WORLD, false), Arguments.of(cddiffDir,
+                                            "Employees/Employees8.cd", "Employees/Employees7.cd",
                                             STA_CLOSED_WORLD, false), Arguments.of(validationDir,
                                                 "cddiff/LibraryV2.cd", "cddiff/LibraryV3.cd",
-                                                STA_CLOSED_WORLD, false), Arguments.of(
-                                                    validationDir, "cddiff/LibraryV4.cd",
-                                                    "cddiff/LibraryV5.cd", STA_CLOSED_WORLD, false),
-        Arguments.of(validationDir, "cd4analysis/ManagementV1.cd", "cd4analysis/ManagementV2.cd",
-            STA_CLOSED_WORLD, true), Arguments.of(validationDir, "cd4analysis/MyCompanyV1.cd",
-                "cd4analysis/MyCompanyV2.cd", STA_CLOSED_WORLD, true), Arguments.of(validationDir,
-                    "cd4analysis/MyCompanyV2.cd", "cd4analysis/MyCompanyV1.cd", STA_CLOSED_WORLD,
-                    false));
+                                                STA_CLOSED_WORLD, true), Arguments.of(validationDir,
+                                                    "cddiff/LibraryV4.cd", "cddiff/LibraryV5.cd",
+                                                    STA_CLOSED_WORLD, true), Arguments.of(
+                                                        validationDir, "cddiff/LibraryV3.cd",
+                                                        "cddiff/LibraryV2.cd", STA_CLOSED_WORLD,
+                                                        false), Arguments.of(validationDir,
+                                                            "cddiff/LibraryV2.cd",
+                                                            "cddiff/LibraryV3.cd", STA_CLOSED_WORLD,
+                                                            false), Arguments.of(validationDir,
+                                                                "cddiff/LibraryV4.cd",
+                                                                "cddiff/LibraryV5.cd",
+                                                                STA_CLOSED_WORLD, false), Arguments
+                                                                    .of(validationDir,
+                                                                        "cd4analysis/ManagementV1.cd",
+                                                                        "cd4analysis/ManagementV2.cd",
+                                                                        STA_CLOSED_WORLD, true),
+        Arguments.of(validationDir, "cd4analysis/MyCompanyV1.cd", "cd4analysis/MyCompanyV2.cd",
+            STA_CLOSED_WORLD, true), Arguments.of(validationDir, "cd4analysis/MyCompanyV2.cd",
+                "cd4analysis/MyCompanyV1.cd", STA_CLOSED_WORLD, false));
   }
-
+  
   @ParameterizedTest
   @MethodSource("witnesses")
   public void testWitnesses(String baseDir, String srcPath, String tgtPath, CDSemantics semantics,
       boolean reduction) {
     dir = baseDir;
     parseModels(srcPath, tgtPath);
-
+    
     if (reduction) {
       ReductionTrafo trafo = new ReductionTrafo();
       trafo.transform(src, tgt);
     }
-
+    
     Syn2SemDiff syn2semdiff = new Syn2SemDiff(src, tgt);
     List<ASTODArtifact> witnesses = syn2semdiff.generateODs(semantics.equals(STA_CLOSED_WORLD)
         || semantics.equals(STA_OPEN_WORLD));
-
+    
     assertFalse(witnesses.isEmpty());
-
+    
     checkDiffWitnesses(semantics, src, tgt, witnesses);
   }
-
+  
   public static Stream<Arguments> witnessesReduction() {
-    return Stream.of(
-        Arguments.of(cddiffDir, "Employees/Employees1.cd", "Employees/Employees2.cd"),
+    return Stream.of(Arguments.of(cddiffDir, "Employees/Employees1.cd", "Employees/Employees2.cd"),
         Arguments.of(validationDir, "Performance/5A.cd", "Performance/5B.cd"), Arguments.of(
             validationDir, "Performance/10A.cd", "Performance/10B.cd"), Arguments.of(validationDir,
                 "Performance/15A.cd", "Performance/15B.cd"), Arguments.of(validationDir,
@@ -221,32 +225,32 @@ public class Syn2SemDiffTest extends SynDiffTestBasis {
                                                                     "Employees/Employees8.cd"),
         Arguments.of(cddiffDir, "DigitalTwins/DigitalTwin3.cd", "DigitalTwins/DigitalTwin2.cd"));
   }
-
+  
   @ParameterizedTest
   @MethodSource("witnessesReduction")
   public void testWitnessesReduction(String baseDir, String srcPath, String tgtPath) {
     dir = baseDir;
     parseModels(srcPath, tgtPath);
-
+    
     ASTCDCompilationUnit srcOriginal = src.deepClone();
     ASTCDCompilationUnit tgtOriginal = tgt.deepClone();
-
+    
     // reduction-based
     ReductionTrafo trafo = new ReductionTrafo();
     trafo.transform(src, tgt);
-
+    
     Syn2SemDiff syn2semdiff = new Syn2SemDiff(src, tgt);
     List<ASTODArtifact> witnesses = syn2semdiff.generateODs(true);
-
+    
     Assertions.assertFalse(witnesses.isEmpty());
-
+    
     checkDiffWitnesses(CDSemantics.STA_CLOSED_WORLD, src, tgt, witnesses);
-
+    
     CD4CodeMill.scopesGenitorDelegator().createFromAST(srcOriginal);
     CD4CodeMill.scopesGenitorDelegator().createFromAST(tgtOriginal);
     checkDiffWitnesses(CDSemantics.STA_OPEN_WORLD, srcOriginal, tgtOriginal, witnesses);
   }
-
+  
   protected void checkDiffWitnesses(CDSemantics semantics, ASTCDCompilationUnit cd1,
       ASTCDCompilationUnit cd2, Collection<ASTODArtifact> witnesses) {
     for (ASTODArtifact od : witnesses) {
@@ -256,5 +260,5 @@ public class Syn2SemDiffTest extends SynDiffTestBasis {
       }
     }
   }
-
+  
 }
--- cddiff/src/test/java/de/monticore/cddiff/syndiff/SyntaxDiffTest.java
+++ cddiff/src/test/java/de/monticore/cddiff/syndiff/SyntaxDiffTest.java
@@ -31,40 +31,40 @@ import static org.junit.jupiter.api.Assertions.assertTrue;
 import static org.junit.jupiter.api.Assertions.fail;
 
 public class SyntaxDiffTest extends SynDiffTestBasis {
-
+  
   @BeforeAll
   public static void init() {
     dir = "src/test/resources/de/monticore/cddiff/";
   }
-
+  
   @Test
   public void testMember1() {
     parseModels("syndiff/MemberDiff/Source1.cd", "syndiff/MemberDiff/Target1.cd");
-
+    
     ASTCDClass cNew = CDTestHelper.getClass("A", src.getCDDefinition());
     ASTCDClass cOld = CDTestHelper.getClass("A", tgt.getCDDefinition());
-
+    
     Assertions.assertNotNull(cNew);
     Assertions.assertNotNull(cOld);
-
+    
     ASTNode attributeNew = CDTestHelper.getAttribute(cNew, "a");
     ASTNode attributeOld = CDTestHelper.getAttribute(cOld, "a");
-
+    
     CDMemberDiff attrDiff = new CDMemberDiff(attributeNew, attributeOld);
-
+    
     assertEquals(new HashSet<>(attrDiff.getBaseDiff()), Set.of(DiffTypes.CHANGED_ATTRIBUTE_TYPE,
         DiffTypes.CHANGED_ATTRIBUTE_MODIFIER));
   }
-
+  
   @Test
   public void testDTs() {
     parseModels("DigitalTwins/DigitalTwin3.cd", "DigitalTwins/DigitalTwin1.cd");
-
+    
     CDSyntaxDiff synDiff = new CDSyntaxDiff(src, tgt, List.of());
-
+    
     new AssertSynDiff(synDiff).assertAddedClasses(4).assertMatchedClasses(4).assertAddedAssocs(2)
         .assertChangedAssocs(3).assertMatchedAssocs(3).assertChangedTypes(2).assertRemainingEmpty();
-
+    
     Map<DiffTypes, Long> expectedDiffTypes = new HashMap<>();
     expectedDiffTypes.put(DiffTypes.ADDED_CLASS, 1L);
     expectedDiffTypes.put(DiffTypes.ADDED_ASSOCIATION, 1L);
@@ -73,21 +73,21 @@ public class SyntaxDiffTest extends SynDiffTestBasis {
     expectedDiffTypes.put(DiffTypes.ADDED_INHERITANCE, 1L);
     expectedDiffTypes.put(DiffTypes.CHANGED_TYPE_EXTENDS, 1L);
     expectedDiffTypes.put(DiffTypes.CHANGED_CLASS_MODIFIER, 1L);
-
+    
     assertEquals(expectedDiffTypes, getDiffTypesCount(synDiff));
   }
-
+  
   @Test
   public void testSyntaxDiff1() {
     parseModels("syndiff/SyntaxDiff/Source1.cd", "syndiff/SyntaxDiff/Target1.cd");
-
+    
     CDSyntaxDiff synDiff = new CDSyntaxDiff(src, tgt, List.of());
-
+    
     new AssertSynDiff(synDiff).assertAddedClasses(1).assertDeletedClasses(1).assertMatchedClasses(3)
         .assertChangedTypes(5).assertAddedAssocs(2).assertDeletedAssocs(2).assertChangedAssocs(2)
         .assertMatchedAssocs(2).assertAddedEnums(1).assertDeletedEnums(1).assertMatchedEnums(1)
         .assertRemainingEmpty();
-
+    
     Map<DiffTypes, Long> expectedDiffTypes = new HashMap<>();
     expectedDiffTypes.put(DiffTypes.DELETED_ASSOCIATION, 1L);
     expectedDiffTypes.put(DiffTypes.DELETED_ENUM, 1L);
@@ -107,20 +107,20 @@ public class SyntaxDiffTest extends SynDiffTestBasis {
     expectedDiffTypes.put(DiffTypes.CHANGED_CLASS_NAME, 1L);
     expectedDiffTypes.put(DiffTypes.CHANGED_CLASS_MODIFIER, 1L);
     expectedDiffTypes.put(DiffTypes.INHERITED_ATTRIBUTE, 1L);
-
+    
     assertEquals(expectedDiffTypes, getDiffTypesCount(synDiff));
   }
-
+  
   @Test
   public void testSyntaxDiff2() {
     parseModels("syndiff/SyntaxDiff/Source2.cd", "syndiff/SyntaxDiff/Target2.cd");
-
+    
     CDSyntaxDiff synDiff = new CDSyntaxDiff(src, tgt, List.of());
-
+    
     new AssertSynDiff(synDiff).assertAddedClasses(2).assertDeletedClasses(2).assertMatchedClasses(2)
         .assertAddedAssocs(1).assertDeletedAssocs(1).assertMatchedAssocs(1).assertAddedEnums(1)
         .assertDeletedEnums(1).assertMatchedEnums(1).assertRemainingEmpty();
-
+    
     Map<DiffTypes, Long> expectedDiffTypes = new HashMap<>();
     expectedDiffTypes.put(DiffTypes.ADDED_CLASS, 1L);
     expectedDiffTypes.put(DiffTypes.ADDED_ASSOCIATION, 1L);
@@ -130,20 +130,20 @@ public class SyntaxDiffTest extends SynDiffTestBasis {
     expectedDiffTypes.put(DiffTypes.DELETED_ASSOCIATION, 1L);
     expectedDiffTypes.put(DiffTypes.DELETED_INHERITANCE, 1L);
     expectedDiffTypes.put(DiffTypes.DELETED_ENUM, 1L);
-
+    
     assertEquals(expectedDiffTypes, getDiffTypesCount(synDiff));
   }
-
+  
   @Test
   public void testSyntaxDiff3() {
     parseModels("syndiff/SyntaxDiff/TechStoreV2.cd", "syndiff/SyntaxDiff/TechStoreV1.cd");
-
+    
     CDSyntaxDiff synDiff = new CDSyntaxDiff(src, tgt, List.of());
-
+    
     new AssertSynDiff(synDiff).assertDeletedClasses(2).assertMatchedClasses(11).assertChangedTypes(
         3).assertAddedAssocs(4).assertDeletedAssocs(2).assertChangedAssocs(5).assertMatchedAssocs(6)
         .assertMatchedEnums(1).assertRemainingEmpty();
-
+    
     Map<DiffTypes, Long> expectedDiffTypes = new HashMap<>();
     expectedDiffTypes.put(DiffTypes.DELETED_ASSOCIATION, 1L);
     expectedDiffTypes.put(DiffTypes.DELETED_INHERITANCE, 1L);
@@ -159,114 +159,114 @@ public class SyntaxDiffTest extends SynDiffTestBasis {
     expectedDiffTypes.put(DiffTypes.CHANGED_ASSOCIATION_CLASS, 2L);
     expectedDiffTypes.put(DiffTypes.CHANGED_ATTRIBUTE_TYPE, 1L);
     expectedDiffTypes.put(DiffTypes.CHANGED_CLASS_NAME, 1L);
-
+    
     assertEquals(expectedDiffTypes, getDiffTypesCount(synDiff));
   }
-
+  
   @Test
   public void testSyntaxDiff4() {
     parseModels("syndiff/SyntaxDiff/TechStoreV9.cd", "syndiff/SyntaxDiff/TechStoreV10.cd");
-
+    
     CDSyntaxDiff synDiff = new CDSyntaxDiff(src, tgt, List.of());
-
+    
     new AssertSynDiff(synDiff).assertMatchedClasses(2).assertDeletedAssocs(1)
         .assertRemainingEmpty();
-
+    
     Map<DiffTypes, Long> expectedDiffTypes = new HashMap<>();
     expectedDiffTypes.put(DiffTypes.DELETED_ASSOCIATION, 1L);
-
+    
     assertEquals(expectedDiffTypes, getDiffTypesCount(synDiff));
   }
-
+  
   @Test
   public void testSyntaxDiff5() {
     parseModels("syndiff/SyntaxDiff/TechStoreV11.cd", "syndiff/SyntaxDiff/TechStoreV12.cd");
-
+    
     CDSyntaxDiff synDiff = new CDSyntaxDiff(src, tgt, List.of());
-
+    
     new AssertSynDiff(synDiff).assertChangedTypes(3).assertMatchedClasses(3).assertRemainingEmpty();
-
+    
     Map<DiffTypes, Long> expectedDiffTypes = new HashMap<>();
     expectedDiffTypes.put(DiffTypes.ADDED_ATTRIBUTE, 1L);
     expectedDiffTypes.put(DiffTypes.INHERITED_ATTRIBUTE, 2L);
-
+    
     assertEquals(expectedDiffTypes, getDiffTypesCount(synDiff));
   }
-
+  
   @Test
   public void testMaCoCo() {
     CDDiffUtil.setUseJavaTypes(true);
     parseMaCoCo("syndiff/SyntaxDiff/MaCoCo_v1.cd", "syndiff/SyntaxDiff/MaCoCo_v2.cd");
-
+    
     CDSyntaxDiff synDiff = new CDSyntaxDiff(src, tgt, List.of());
-
+    
     new AssertSynDiff(synDiff).assertAddedClasses(1).assertAddedEnums(1).assertAddedAssocs(1)
         .assertMatchedAssocs(91).assertMatchedClasses(94).assertMatchedEnums(45)
         .assertRemainingEmpty();
-
+    
     Map<DiffTypes, Long> expectedDiffTypes = new HashMap<>();
     expectedDiffTypes.put(DiffTypes.ADDED_CLASS, 1L);
     expectedDiffTypes.put(DiffTypes.ADDED_ENUM, 1L);
     expectedDiffTypes.put(DiffTypes.ADDED_ASSOCIATION, 1L);
     expectedDiffTypes.put(DiffTypes.ADDED_INHERITANCE, 1L);
-
+    
     assertEquals(expectedDiffTypes, getDiffTypesCount(synDiff));
-
+    
     // Conformance Checking without stereotype mapping constitutes a refinement check
     boolean conform = new CDConformanceChecker(Set.of(CDConfParameter.STEREOTYPE_MAPPING,
         CDConfParameter.NAME_MAPPING, CDConfParameter.SRC_TARGET_ASSOC_MAPPING,
         CDConfParameter.ALLOW_CARD_RESTRICTION)).checkConformance(src, tgt, "incarnates");
-
+    
     assertTrue(conform);
-
+    
     // Syn2SemDiff produces correct diff-witnesses
     Syn2SemDiff semDiff = new Syn2SemDiff(src, tgt);
     OD2CDMatcher matcher = new OD2CDMatcher();
     List<ASTODArtifact> ods = semDiff.generateODs(false);
-
+    
     assertFalse(ods.isEmpty());
     assertTrue(semDiff.generateODs(false).stream().allMatch(od -> matcher.checkIfDiffWitness(
         CDSemantics.SIMPLE_CLOSED_WORLD, src, tgt, od)));
-
+    
     synDiff = new CDSyntaxDiff(tgt, src, List.of());
     new AssertSynDiff(synDiff).assertDeletedClasses(1).assertDeletedEnums(1).assertDeletedAssocs(1)
         .assertMatchedAssocs(91).assertMatchedClasses(94).assertMatchedEnums(45)
         .assertRemainingEmpty();
-
+    
     expectedDiffTypes = new HashMap<>();
     expectedDiffTypes.put(DiffTypes.DELETED_CLASS, 1L);
     expectedDiffTypes.put(DiffTypes.DELETED_ENUM, 1L);
     expectedDiffTypes.put(DiffTypes.DELETED_ASSOCIATION, 1L);
     expectedDiffTypes.put(DiffTypes.DELETED_INHERITANCE, 1L);
-
+    
     assertEquals(expectedDiffTypes, getDiffTypesCount(synDiff));
-
+    
     // Conformance Checking without stereotype mapping constitutes a refinement check
     conform = new CDConformanceChecker(Set.of(CDConfParameter.STEREOTYPE_MAPPING,
         CDConfParameter.NAME_MAPPING, CDConfParameter.SRC_TARGET_ASSOC_MAPPING,
         CDConfParameter.ALLOW_CARD_RESTRICTION)).checkConformance(tgt, src, "incarnates");
-
+    
     assertFalse(conform);
-
+    
     // Syn2SemDiff produces correct diff-witnesses
     semDiff = new Syn2SemDiff(tgt, src);
     ods = semDiff.generateODs(false);
-
+    
     assertTrue(ods.isEmpty());
     assertTrue(Log.getFindings().isEmpty());
   }
-
+  
   @Test
   public void testMaCoCo2() {
     CDDiffUtil.setUseJavaTypes(true);
-    parseModels("MaCoCo_Failing_1.cd", "MaCoCo_Failing_2.cd");
-
+    parseMaCoCo("syndiff/SyntaxDiff/MaCoCo_Failing_1.cd", "syndiff/SyntaxDiff/MaCoCo_Failing_2.cd");
+    
     CDSyntaxDiff synDiff = new CDSyntaxDiff(src, tgt, List.of());
     SyntaxDiffPrinter sb = new SyntaxDiffPrinter(synDiff);
     Log.println(sb.printDiff());
-
+    
     assertEquals(1, synDiff.changedTypes().size());
-
+    
     assertTrue(synDiff.getAddedClasses().isEmpty());
     assertTrue(synDiff.getDeletedClasses().isEmpty());
     assertTrue(synDiff.getAddedInterfaces().isEmpty());
@@ -276,21 +276,21 @@ public class SyntaxDiffTest extends SynDiffTestBasis {
     assertTrue(synDiff.getAddedAssocs().isEmpty());
     assertTrue(synDiff.getDeletedAssocs().isEmpty());
     assertTrue(synDiff.getChangedAssocs().isEmpty());
-
+    
     // Syn2SemDiff produces correct diff-witnesses
     Syn2SemDiff semDiff = new Syn2SemDiff(src, tgt);
     OD2CDMatcher matcher = new OD2CDMatcher();
     List<ASTODArtifact> ods = semDiff.generateODs(false);
-
+    
     assertFalse(ods.isEmpty());
     assertTrue(semDiff.generateODs(false).stream().allMatch(od -> matcher.checkIfDiffWitness(
         CDSemantics.SIMPLE_CLOSED_WORLD, src, tgt, od)));
-
+    
     synDiff = new CDSyntaxDiff(tgt, src, List.of());
     sb = new SyntaxDiffPrinter(synDiff);
-
+    
     assertEquals(1, synDiff.changedTypes().size());
-
+    
     assertTrue(synDiff.getAddedClasses().isEmpty());
     assertTrue(synDiff.getDeletedClasses().isEmpty());
     assertTrue(synDiff.getAddedInterfaces().isEmpty());
@@ -300,16 +300,16 @@ public class SyntaxDiffTest extends SynDiffTestBasis {
     assertTrue(synDiff.getAddedAssocs().isEmpty());
     assertTrue(synDiff.getDeletedAssocs().isEmpty());
     assertTrue(synDiff.getChangedAssocs().isEmpty());
-
+    
     Log.println(sb.printDiff());
     // Syn2SemDiff produces no diff-witnesses
     semDiff = new Syn2SemDiff(tgt, src);
     ods = semDiff.generateODs(false);
-
+    
     assertTrue(ods.isEmpty());
     assertTrue(Log.getFindings().isEmpty());
   }
-
+  
   public void parseMaCoCo(String concrete, String ref) {
     try {
       Optional<ASTCDCompilationUnit> src = CD4CodeMill.parser().parseCDCompilationUnit(dir
@@ -325,11 +325,11 @@ public class SyntaxDiffTest extends SynDiffTestBasis {
         fail(String.format("Parsing src: '%s', tgt: '%s'.", src.isPresent() ? "success" : "failure",
             tgt.isPresent() ? "success" : "failure"));
       }
-
+      
     }
     catch (IOException e) {
       fail(e.getMessage());
     }
   }
-
+  
 }
