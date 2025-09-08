16a735324b7f477db98e257610bcb940d40d8325
Lukas Kronast <lukas.kronast@rwth-aachen.de>
2025-09-08 13:27:30 +0200

    cd4a conversion

--- cddiff/src/test/java/de/monticore/cddiff/cdscoring/LLMCDScoringTest.java
+++ cddiff/src/test/java/de/monticore/cddiff/cdscoring/LLMCDScoringTest.java
@@ -68,7 +68,6 @@ public class LLMCDScoringTest extends SynDiffTestBasis {
   @Test
   public void testSpecificSyntheticArtifacts() {
     // ToDo: Enum Pre-Matches
-    // ToDo: Ignore or reduce impact of no Element Matches
     // ToDo: Increase name importance and maybe adjust embedding accordingly
     score("SyntheticArtifacts/unconstrained_code_block_31.cd4c", "SyntheticArtifacts/unconstrained_code_block_23.cd4c", 5, 0.5, true);
     score("SyntheticArtifacts/unconstrained_code_block_29.cd4c", "SyntheticArtifacts/unconstrained_code_block_26.cd4c", 5, 0.5, true);
