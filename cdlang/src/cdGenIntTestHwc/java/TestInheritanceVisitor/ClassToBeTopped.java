7953f9dd1037d0c550c2d01e488769c8927a32ac
Alex Lüpges <luepges@se-rwth.de>
2025-10-13 17:33:02 +0200

    delete classes

--- cdlang/src/cdGenIntTestHwc/java/TestInheritanceVisitor/ClassToBeTopped.java
+++
@@ -1,6 +0,0 @@
-/* (c) https://github.com/MontiCore/monticore */
-package TestInheritanceVisitor;
-
-public class ClassToBeTopped extends ClassToBeToppedTOP {
-  
-}
--- cdlang/src/cdGenIntTestHwc/java/TestVisitor/ClassToBeTopped.java
+++
@@ -1,6 +0,0 @@
-/* (c) https://github.com/MontiCore/monticore */
-package TestVisitor;
-
-public class ClassToBeTopped extends ClassToBeToppedTOP {
-  
-}
