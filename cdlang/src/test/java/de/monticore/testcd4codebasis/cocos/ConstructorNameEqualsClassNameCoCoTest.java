2f9c6919478c0b0d17bee097c0be0ec492b019cb
Vanol Nguemo Tadoum <nguemovanol@gmail.com>
2026-05-05 14:10:38 +0200

    spotless

--- cdlang/src/test/java/de/monticore/testcd4codebasis/cocos/ConstructorNameEqualsClassNameCoCoTest.java
+++ cdlang/src/test/java/de/monticore/testcd4codebasis/cocos/ConstructorNameEqualsClassNameCoCoTest.java
@@ -14,40 +14,40 @@ import org.junit.jupiter.api.AfterEach;
 import org.junit.jupiter.api.Test;
 
 public class ConstructorNameEqualsClassNameCoCoTest extends CD4CodeBasisTestBasis {
-
+  
   @AfterEach
   public void after() {
     Log.getFindings().clear();
   }
-
+  
   @Test
   public void testValid() throws IOException {
     // Given
     coCoChecker.addCoCo(new ConstructorNameEqualsClassNameCoCo());
-
+    
     // When
-    final ASTCDCompilationUnit ast = p.parse(
-        getFilePath("cd4codebasis/cocos/ConstructorNameEqualsClassNameValid.cd")).orElseThrow();
+    final ASTCDCompilationUnit ast = p.parse(getFilePath(
+        "cd4codebasis/cocos/ConstructorNameEqualsClassNameValid.cd")).orElseThrow();
     coCoChecker.checkAll(ast);
-
+    
     // Then
     assertTrue(Log.getFindings().isEmpty(), "Valid constructors should not produce errors");
   }
-
+  
   @Test
   public void testInvalid() throws IOException {
     // Given
     coCoChecker.addCoCo(new ConstructorNameEqualsClassNameCoCo());
-
+    
     // When
-    final ASTCDCompilationUnit ast = p.parse(
-        getFilePath("cd4codebasis/cocos/ConstructorNameEqualsClassNameInvalid.cd")).orElseThrow();
+    final ASTCDCompilationUnit ast = p.parse(getFilePath(
+        "cd4codebasis/cocos/ConstructorNameEqualsClassNameInvalid.cd")).orElseThrow();
     coCoChecker.checkAll(ast);
-
+    
     // Then
     assertEquals(2, Log.getFindings().size(), "Invalid constructors should produce 2 errors");
     assertTrue(Log.getFindings().get(0).getMsg().startsWith("0xCDC50"));
     assertTrue(Log.getFindings().get(1).getMsg().startsWith("0xCDC50"));
   }
-
+  
 }
