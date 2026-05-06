32a6daad3af0de54c304da835156fc4100289c98
Vanol Nguemo Tadoum <nguemovanol@gmail.com>
2026-05-06 14:42:55 +0200

    solve spotless

--- cdlang/src/main/java/de/monticore/cdbasis/cocos/ConstructorNameEqualsClassNameCoCo.java
+++ cdlang/src/main/java/de/monticore/cdbasis/cocos/ConstructorNameEqualsClassNameCoCo.java
@@ -10,16 +10,16 @@ import de.se_rwth.commons.logging.Log;
  * Validates that all constructors of a CD class have the same name as the class.
  */
 public class ConstructorNameEqualsClassNameCoCo implements CDBasisASTCDClassCoCo {
-
+  
   public static final String ERROR_CODE = "0xCDC50";
   public static final String ERROR_MESSAGE = ERROR_CODE
       + ": Invalid constructor name '%s' in class '%s'. "
       + "Constructors must have the same name as their class.";
-
+  
   @Override
   public void check(ASTCDClass node) {
     String className = node.getName();
-
+    
     for (ASTCDConstructor constructor : node.getCDConstructorList()) {
       if (!constructor.getName().equals(className)) {
         Log.error(String.format(ERROR_MESSAGE, constructor.getName(), className), constructor
@@ -27,4 +27,5 @@ public class ConstructorNameEqualsClassNameCoCo implements CDBasisASTCDClassCoCo
       }
     }
   }
+  
 }
