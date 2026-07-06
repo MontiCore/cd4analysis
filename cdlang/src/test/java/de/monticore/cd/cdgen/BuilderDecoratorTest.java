22aec90c8c540da2a47fd40e73dda5d9b9671cd4
Vanol Nguemo Tadoum <nguemovanol@gmail.com>
2026-07-07 00:51:49 +0200

    resolve conflicts

--- cdlang/src/test/java/de/monticore/cd/cdgen/BuilderDecoratorTest.java
+++ cdlang/src/test/java/de/monticore/cd/cdgen/BuilderDecoratorTest.java
@@ -9,6 +9,7 @@ import de.monticore.generating.GeneratorSetup;
 import de.monticore.generating.templateengine.GlobalExtensionManagement;
 import de.monticore.io.paths.MCPath;
 import de.monticore.runtime.junit.MCAssertions;
+import de.se_rwth.commons.logging.Log;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.Assertions;
 import java.nio.file.Paths;
@@ -71,9 +72,12 @@ class BuilderDecoratorTest extends AbstractDecoratorTest {
     
     super.doTest(opt.get());
     
-    for (int i = 0; i < 7; i++) // Test, that the warning about missing setters is present
-      MCAssertions.assertHasFinding(f -> f.getMsg().startsWith("Requested setter of TestBuilder")
-          && f.isWarning());
+    // Test that the warnings about missing setters are present.
+    Assertions.assertEquals(7, Log.getFindings().stream().filter(f -> f.getMsg().startsWith(
+        "Requested setter of TestBuilder") && f.isWarning()).count());
+    Assertions.assertTrue(Log.getFindings().stream().allMatch(f -> f.getMsg().startsWith(
+        "Requested setter of TestBuilder") && f.isWarning()));
+    Log.clearFindings();
   }
   
   @Test
