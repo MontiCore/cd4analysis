a62a28edec89166280363c9fa4861412977144ac
Janik Rapp <rapp@se-rwth.de>
2026-07-14 11:56:29 +0200

    fix AddAttributeTest: strip whitespace introduced by parent comments

--- cdlang/src/test/javatrafo/de/monticore/trafo/AddAttributeTest.java
+++ cdlang/src/test/javatrafo/de/monticore/trafo/AddAttributeTest.java
@@ -1,43 +1,1 @@
-/* (c) https://github.com/MontiCore/monticore */
-package de.monticore.trafo;
-
-import static org.junit.jupiter.api.Assertions.assertEquals;
-import static org.junit.jupiter.api.Assertions.assertTrue;
-
-import de.monticore.cd4code.CD4CodeMill;
-import de.monticore.cdbasis._ast.ASTCDCompilationUnit;
-import de.monticore.literals.mccommonliterals.MCCommonLiteralsMill;
-import de.monticore.tf.AddAttribute;
-import java.io.IOException;
-import java.util.Optional;
-import org.junit.jupiter.api.BeforeAll;
-import org.junit.jupiter.api.Test;
-
-public class AddAttributeTest {
-  
-  @BeforeAll
-  public static void init() {
-    CD4CodeMill.reset();
-    CD4CodeMill.init();
-  }
-  
-  @Test
-  public void testAddAttr() throws IOException {
-    String input = "src/test/resources/de/monticore/trafo/MoveAttrAB.cd";
-    Optional<ASTCDCompilationUnit> ast = CD4CodeMill.parser().parse(input);
-    
-    assertTrue(ast.isPresent());
-    
-    AddAttribute addAttribute = new AddAttribute(ast.get());
-    assertTrue(addAttribute.doPatternMatching());
-    
-    addAttribute.doReplacement();
-    
-    assertEquals("boolean", ast.get().getCDDefinition().getCDClassesList().get(0)
-        .getCDAttributeList().get(1).getMCType().printType());
-    String pp = MCCommonLiteralsMill.prettyPrint(ast.get().getCDDefinition().getCDClassesList().get(
-        0).getCDAttributeList().get(1).getInitial(), true);
-    assertEquals("true", pp);
-  }
-  
-}
+/* (c) https://github.com/MontiCore/monticore */package de.monticore.trafo;import static org.junit.jupiter.api.Assertions.assertEquals;import static org.junit.jupiter.api.Assertions.assertTrue;import de.monticore.cd4code.CD4CodeMill;import de.monticore.cdbasis._ast.ASTCDCompilationUnit;import de.monticore.literals.mccommonliterals.MCCommonLiteralsMill;import de.monticore.tf.AddAttribute;import java.io.IOException;import java.util.Optional;import org.junit.jupiter.api.BeforeAll;import org.junit.jupiter.api.Test;public class AddAttributeTest {    @BeforeAll  public static void init() {    CD4CodeMill.reset();    CD4CodeMill.init();  }    @Test  public void testAddAttr() throws IOException {    String input = "src/test/resources/de/monticore/trafo/MoveAttrAB.cd";    Optional<ASTCDCompilationUnit> ast = CD4CodeMill.parser().parse(input);        assertTrue(ast.isPresent());        AddAttribute addAttribute = new AddAttribute(ast.get());    assertTrue(addAttribute.doPatternMatching());        addAttribute.doReplacement();        assertEquals("boolean", ast.get().getCDDefinition().getCDClassesList().get(0)        .getCDAttributeList().get(1).getMCType().printType());    String pp = MCCommonLiteralsMill.prettyPrint(ast.get().getCDDefinition().getCDClassesList().get(        0).getCDAttributeList().get(1).getInitial(), true);    assertEquals("true", pp.strip());  }  }
