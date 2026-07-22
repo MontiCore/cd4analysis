4e43d5a5fc7e8af7c6108b1782fa34c8e199cb3d
Max Philip Stachon <max.stachon@rwth-aachen.de>
2026-07-22 14:00:50 +0200

    fix asserts

--- cdlang/src/test/java/de/monticore/cdgen/cocos/CDAssociationUniqueTest.java
+++ cdlang/src/test/java/de/monticore/cdgen/cocos/CDAssociationUniqueTest.java
@@ -8,7 +8,7 @@ import de.monticore.cd4code.cocos.AbstractJavaGenCoCoTest;
 import org.junit.jupiter.api.Test;
 
 public class CDAssociationUniqueTest extends AbstractJavaGenCoCoTest {
-
+  
   // This CoCo checks for unique association names within a single class diagram.
   // Assumed to be available from the de.monticore.cdassociation.cocos package.
   @Override
@@ -17,71 +17,71 @@ public class CDAssociationUniqueTest extends AbstractJavaGenCoCoTest {
     checker.addCoCo(new CDAssociationUnique());
     return checker;
   }
-
+  
   // The error code is assumed, as the definition for CDAssociationUnique was not provided.
   private static final String ERROR_CODE = "0xCDCE1";
-
+  
   @Test
   public void testUniqueNames() throws IOException {
     String model = "classdiagram UniqueAssocs {" + "  class A; class B;" + "  association A -> B;"
         + "  association A (l) -> (r) B;" + "}";
     runTest(model, false);
   }
-
+  
   @Test
   public void testDuplicatesWithExplicitRoles() throws IOException {
     String model = "classdiagram DuplicateAssocs {" + "  class A; class B;"
         + "  association A -> B;" + "  association A -> (b) B;" + "}";
-    runTest(model, false);
+    runTestForErrorCode(model, ERROR_CODE);
   }
-
+  
   @Test
   public void testDuplicatesWithImplicitRoles() throws IOException {
     String model = "classdiagram DuplicateAssocs {" + "  class A; class B;"
         + "  association A -> B;" + "  association A -> B;" + "}";
     runTestForErrorCode(model, ERROR_CODE);
   }
-
+  
   @Test
   public void testDuplicatesInReverse() throws IOException {
     String model = "classdiagram DuplicateAssocs {" + "  class A; class B;"
         + "  association A -> B;" + "  association B <- A;" + "}";
-    runTest(model, false);
+    runTestForErrorCode(model, ERROR_CODE);
   }
-
+  
   @Test
   public void testUniqueAssocName() throws IOException {
     String model = "classdiagram UniqueAssocs {" + "  class A; class B;"
         + "  association assoc1 A -> B;" + "  association assoc2 A -> B;" + "}";
     runTest(model, false);
   }
-
+  
   @Test
   public void testNavNotMatch() throws IOException {
     String model = "classdiagram DuplicateAssocs {" + "  class A; class B;"
         + "  association A -> (r) B;" + "  association A <- (r) B;" + "}";
     runTest(model, false);
   }
-
+  
   @Test
   public void testNavNotMatchReverse() throws IOException {
     String model = "classdiagram DuplicateAssocs {" + "  class A; class B;"
         + "  association A -> (r) B;" + "  association B (r) -> A;" + "}";
     runTest(model, false);
   }
-
+  
   @Test
   public void testNavNotMatchBiDir() throws IOException {
     String model = "classdiagram DuplicateAssocs {" + "  class A; class B;"
         + "  association A -- (r) B;" + "  association A <-> (r) B;" + "}";
     runTest(model, false);
   }
-
+  
   @Test
   public void testDuplicatesDifferentClassesWithExplicitRoles() throws IOException {
     String model = "classdiagram DuplicateAssocs {" + "  class A; class B; class C;"
-      + "  association A -> (k) B;" + "  association A -> (k) C;" + "}";
+        + "  association A -> (k) B;" + "  association A -> (k) C;" + "}";
     runTestForErrorCode(model, ERROR_CODE);
   }
-
+  
 }
