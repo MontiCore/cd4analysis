b76751e92e256caa0d2e1e003900db444ec63ff1
Christoph Lütticke <christoph.luetticke@rwth-aachen.de>
2026-08-28 15:41:37 +0200

    formatting

--- cdlang/src/main/java/de/monticore/cdgen/CDGenTool.java
+++ cdlang/src/main/java/de/monticore/cdgen/CDGenTool.java
@@ -378,29 +378,28 @@ public class CDGenTool extends CD4CodeTool {
   public Options addAdditionalOptions(Options options) {
     
     options.addOption(Option.builder("c").longOpt("checkcococs").desc(
-        "Checks all CoCos on the given mode.").build());
+        "Checks all CoCos on the given mode.").get());
     
     options.addOption(Option.builder("o").longOpt("output").argName("dir").hasArg().desc(
-        "Sets the output path.").build());
+        "Sets the output path.").get());
     
     options.addOption(Option.builder("ct").longOpt("configtemplate").hasArg().argName("template")
-        .desc("Sets a template for configuration.").build());
+        .desc("Sets a template for configuration.").get());
     
     options.addOption(Option.builder("fp").longOpt("template").hasArg().argName("path").desc(
-        "Sets the path for additional templates.").build());
+        "Sets the path for additional templates.").get());
     
     options.addOption(Option.builder("hwc").longOpt("handwrittencode").hasArg().argName("hwcpath")
-        .desc("Sets the path for additional, handwritten classes.").build());
+        .desc("Sets the path for additional, handwritten classes.").get());
     
     options.addOption(Option.builder("c2mc").longOpt("class2mc").desc(
-        "Enables to resolve java classes in the model path").build());
+        "Enables to resolve java classes in the model path").get());
     
     options.addOption(Option.builder("cliconfig").desc("Configures additional").hasArgs().argName(
-        "fqn:key[=value]").build());
+        "fqn:key[=value]").get());
     
-    options.addOption(org.apache.commons.cli.Option.builder("sd").longOpt("symboltabledecorated")
-        .argName("file").hasArg().desc(
-            "Serializes the decorated symbol table of the given artifact.").build());
+    options.addOption(Option.builder("sd").longOpt("symboltabledecorated").argName("file").hasArg()
+        .desc("Serializes the decorated symbol table of the given artifact.").get());
     
     return options;
   }
