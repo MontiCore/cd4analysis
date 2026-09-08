14504715ab5025ddfd113fc7d2c87751fc60f367
Alex Lüpges <luepges@se-rwth.de>
2026-09-08 16:54:44 +0200

    fmt

--- cdtool/src/test/java/de/monticore/ExampleCommandTest.java
+++ cdtool/src/test/java/de/monticore/ExampleCommandTest.java
@@ -177,7 +177,9 @@ public class ExampleCommandTest extends OutTestBasis {
     // Then
     assertEquals(4, Log.getFindingsCount(), "Actual findings: " + Log.getFindings().toString());
     assertEquals("0xA0324 Cannot find symbol Address", Log.getFindings().get(0).getMsg());
-    assertEquals("0xCDC41: Could not resolve import `MyAddress.Address` within the input models or symbol path.", Log.getFindings().get(3).getMsg());
+    assertEquals(
+        "0xCDC41: Could not resolve import `MyAddress.Address` within the input models or symbol path.",
+        Log.getFindings().get(3).getMsg());
     
     Log.clearFindings();
   }
